5ba0e9e32ee3235e988309a1d6402325ee4f71de
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:08:32 -0500

    Create 837-MostCommonWord

---
+++ 837-MostCommonWord/837-MostCommonWord.py
@@ -0,0 +1,24 @@
+# Last updated: 8/20/2026, 2:08:31 AM
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        
+        normalized_str = ''.join([c.lower() if c.isalnum() else ' ' for c in paragraph])
+
+        words = normalized_str.split()
+        print(words)
+        word_count = {}
+
+        for word in words:
+            if word not in set(banned):
+                if word in word_count:
+                    word_count[word] += 1
+                else:
+                    word_count[word] = 1
+
+        max_freq = max(word_count.values())
+        for k, v in word_count.items():
+            if v == max_freq:
+                return k
+
+        
+

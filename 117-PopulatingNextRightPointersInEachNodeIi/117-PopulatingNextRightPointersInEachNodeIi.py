8489924ba9df2e6edd055023e167a256cf10680e
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:17:42 -0500

    Create 117-PopulatingNextRightPointersInEachNodeIi

---
+++ 117-PopulatingNextRightPointersInEachNodeIi/117-PopulatingNextRightPointersInEachNodeIi.py
@@ -0,0 +1,49 @@
+# Last updated: 8/20/2026, 2:17:41 AM
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        
+        def helper(child, prev, leftmost):
+            
+            if child:
+            
+                if prev:
+                    prev.next = child
+
+                else:
+                    leftmost = child
+
+                prev = child
+                
+            return prev, leftmost
+        
+        
+        if not root: return None
+        
+        leftmost = root
+        
+        while leftmost:
+            prev, curr = None, leftmost
+            leftmost = None
+            
+            while curr:
+                prev, leftmost = helper(curr.left, prev, leftmost)
+                prev, leftmost = helper(curr.right, prev, leftmost)
+                
+                curr = curr.next
+        
+        return root
+                
+                
+            
+            
+        

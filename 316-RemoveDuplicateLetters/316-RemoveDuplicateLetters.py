b6202f787cd453181c36d6733af842a656f2cbc6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:14:13 -0500

    Create 316-RemoveDuplicateLetters

---
+++ 316-RemoveDuplicateLetters/316-RemoveDuplicateLetters.py
@@ -0,0 +1,26 @@
+# Last updated: 8/20/2026, 2:14:13 AM
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        
+        stack = []
+        visited = set()
+        
+        hm = {}
+        for i, char in enumerate(s):
+            hm[char] = i
+            
+        for i in range(len(s)):
+            
+            if s[i] not in visited:
+                
+                while stack and s[i] < stack[-1] and i < hm[stack[-1]]:
+                    char = stack.pop()
+                    if char in visited:
+                        visited.remove(char)
+                        
+                visited.add(s[i])
+                stack.append(s[i])
+                
+        return "".join(stack)
+            
+        

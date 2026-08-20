6105ce906f0bc42a3f2040de4061dc04d63edbca
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:06:48 -0500

    Create 1039-FindTheTownJudge

---
+++ 1039-FindTheTownJudge/1039-FindTheTownJudge.py
@@ -0,0 +1,18 @@
+# Last updated: 8/20/2026, 2:06:48 AM
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        
+        degrees = [0] * (n+1)
+        
+        for a, b in trust:
+            degrees[a] -= 1
+            degrees[b] += 1
+            
+        
+        for i in range(1, len(degrees)):
+            if degrees[i] == n - 1:
+                return i
+            
+        return -1
+            
+        

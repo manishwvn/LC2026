a67b57745fa07e6c0258ea9096153c2af9ccf5e3
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:17:39 -0500

    Create 119-PascalsTriangleIi

---
+++ 119-PascalsTriangleIi/119-PascalsTriangleIi.py
@@ -0,0 +1,15 @@
+# Last updated: 8/20/2026, 2:17:38 AM
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+
+        result = [1]
+
+        for i in range(rowIndex):
+            next_row = [0] * (len(result) + 1)
+            for j in range(len(result)):
+                next_row[j] += result[j]
+                next_row[j+1] += result[j]
+            result = next_row
+
+        return result
+        

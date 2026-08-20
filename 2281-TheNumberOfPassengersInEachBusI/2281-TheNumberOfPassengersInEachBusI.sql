0f6167c51ac59e2110b437a085df17357f191e73
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:57:56 -0500

    Create 2281-TheNumberOfPassengersInEachBusI

---
+++ 2281-TheNumberOfPassengersInEachBusI/2281-TheNumberOfPassengersInEachBusI.sql
@@ -0,0 +1,19 @@
+-- Last updated: 8/20/2026, 1:57:56 AM
+with used_bus as (select passenger_id, min(b.arrival_time) as m
+			from passengers p join buses b 
+			on p.arrival_time <= b.arrival_time
+			group by passenger_id)
+
+
+select 
+    bus_id, count(used_bus.m) as passengers_cnt
+from 
+    buses 
+left join 
+    used_bus 
+on 
+    (used_bus.m = buses.arrival_time)
+group by 
+    bus_id
+order by
+    bus_id

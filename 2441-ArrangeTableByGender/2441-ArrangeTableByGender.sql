4e2daa9bc7021efcf595e82eb59e044917695636
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:56:57 -0500

    Create 2441-ArrangeTableByGender

---
+++ 2441-ArrangeTableByGender/2441-ArrangeTableByGender.sql
@@ -0,0 +1,18 @@
+-- Last updated: 8/20/2026, 1:56:57 AM
+with cte as (select
+    *,
+    rank() over(partition by gender order by user_id) as rnk1,
+    case
+        when gender = 'female' then 1
+        when gender = 'other' then 2
+        else 3 
+    end as rnk2
+from
+    genders)
+
+select
+    user_id,
+    gender
+from
+    cte
+order by rnk1, rnk2;

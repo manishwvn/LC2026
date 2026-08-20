65154cfb24dbbe9f541b107ed6e785ca5ac7225c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:15:00 -0500

    Create 250-CountUnivalueSubtrees

---
+++ 250-CountUnivalueSubtrees/250-CountUnivalueSubtrees.py
@@ -0,0 +1,26 @@
+# Last updated: 8/20/2026, 2:15:00 AM
+class Solution:
+    def countUnivalSubtrees(self, root: Optional[TreeNode]) -> int:
+        result = 0
+
+        def dfs(node):
+            nonlocal result
+            if not node:
+                return True
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            if not left or not right:
+                return False
+
+            if node.left and node.left.val != node.val:
+                return False
+            if node.right and node.right.val != node.val:
+                return False
+
+            result += 1
+            return True
+
+        dfs(root)
+        return result

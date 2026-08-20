22c2b0b37745c6af19264858f30bd5d88de9a7d2
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:16:13 -0500

    Create 185-DepartmentTopThreeSalaries

---
+++ 185-DepartmentTopThreeSalaries/185-DepartmentTopThreeSalaries.sql
@@ -0,0 +1,20 @@
+-- Last updated: 8/20/2026, 2:16:13 AM
+select
+    department as Department,
+    name as Employee,
+    salary as Salary 
+from
+    (select
+        e.*, d.name as department, 
+        dense_rank() over(partition by d.id order by e.salary desc) as rnk
+    from
+        department d 
+    join
+        employee e 
+    on
+        d.id = e.departmentId
+    ) t
+where
+    t.rnk <=3;
+
+

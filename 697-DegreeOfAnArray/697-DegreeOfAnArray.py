d1500f80ba90c8604a14f25a1338dceea175510a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:09:42 -0500

    Create 697-DegreeOfAnArray

---
+++ 697-DegreeOfAnArray/697-DegreeOfAnArray.py
@@ -0,0 +1,21 @@
+# Last updated: 8/20/2026, 2:09:42 AM
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        
+        counts = Counter(nums)
+        degree = max(counts.values())
+        
+        left, right = {}, {}
+        
+        for i, num in enumerate(nums):
+            if num not in left: left[num] = i
+            right[num] = i
+            
+        result = len(nums)
+        
+        for num in counts:
+            if counts[num] == degree:
+                result = min(result, right[num] - left[num] + 1)
+                
+        return result
+                

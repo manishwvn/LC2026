d346f6d6adfd3f5990248d9dad4199b27330be11
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:56:08 -0500

    Create 2599-TakeKOfEachCharacterFromLeftAndRight

---
+++ 2599-TakeKOfEachCharacterFromLeftAndRight/2599-TakeKOfEachCharacterFromLeftAndRight.py
@@ -0,0 +1,27 @@
+# Last updated: 8/20/2026, 1:56:07 AM
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+
+        #get total counts of a, b, c:
+        total = [0, 0, 0]
+
+        for char in s:
+            total[ord(char) - ord('a')] += 1
+
+        #check if s is valid
+        if min(total) < k: return -1
+        
+        #sliding window
+
+        res = len(s)
+        l = 0 
+        for r in range(len(s)):
+            total[ord(s[r]) - ord('a')] -= 1
+
+            while min(total) < k:
+                total[ord(s[l]) - ord('a')] += 1
+                l += 1
+
+            res = min(res, len(s) - (r - l + 1))
+
+        return res

e03f7ad1e67cf085e9d97b0d35787b8e5ea0eb01
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:57:23 -0500

    Create 2371-TheUsersThatAreEligibleForDiscount

---
+++ 2371-TheUsersThatAreEligibleForDiscount/2371-TheUsersThatAreEligibleForDiscount.sql
@@ -0,0 +1,16 @@
+-- Last updated: 8/20/2026, 1:57:23 AM
+CREATE PROCEDURE getUserIDs(startDate DATE, endDate DATE, minAmount INT)
+BEGIN
+        select
+            distinct user_id
+        from
+            purchases
+        where
+            amount >= minAmount
+        and
+            time_stamp >= startDate
+        and
+            time_stamp <= endDate
+        order by
+            user_id;
+END

7728468e3a8e13e1de4f07932a24d1627fb44ad6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:08:08 -0500

    Create 883-CarFleet

---
+++ 883-CarFleet/883-CarFleet.py
@@ -0,0 +1,19 @@
+# Last updated: 8/20/2026, 2:08:08 AM
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        pairs = [[position[i], speed[i]] for i in range(len(position))]
+        pairs.sort(reverse = True)
+
+        res = prev = 0
+
+        for pair in pairs:
+            p, s = pair
+            time = (target - p) / s
+
+            if prev < time:
+                res += 1
+                prev = time
+
+        return res
+

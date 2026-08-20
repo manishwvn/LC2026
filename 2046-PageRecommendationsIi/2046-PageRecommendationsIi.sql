a87935b254ad16892a7e0656167a4f0bd4a43a2d
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:59:23 -0500

    Create 2046-PageRecommendationsIi

---
+++ 2046-PageRecommendationsIi/2046-PageRecommendationsIi.sql
@@ -0,0 +1,35 @@
+-- Last updated: 8/20/2026, 1:59:23 AM
+WITH t1 AS (
+    SELECT
+        user1_id user_id,
+        user2_id friend_id
+    FROM
+        Friendship
+    UNION
+    SELECT
+        user2_id user_id,
+        user1_id friend_id
+    FROM
+        Friendship
+)
+SELECT
+    t1.user_id,
+    l1.page_id,
+    COUNT(1) friends_likes
+FROM
+    t1
+JOIN
+    Likes l1
+ON
+    t1.friend_id = l1.user_id
+LEFT JOIN
+    Likes l2
+ON
+    t1.user_id = l2.user_id
+AND
+    l2.page_id = l1.page_id
+WHERE
+    l2.page_id IS NULL
+GROUP BY
+    t1.user_id,
+    l1.page_id

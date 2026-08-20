c65990348ea6362db1f145c296de937696f492e3
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:02:26 -0500

    Create 1565-EvaluateBooleanExpression

---
+++ 1565-EvaluateBooleanExpression/1565-EvaluateBooleanExpression.sql
@@ -0,0 +1,23 @@
+-- Last updated: 8/20/2026, 2:02:26 AM
+SELECT
+    E.LEFT_OPERAND AS 'left_operand',
+    E.OPERATOR AS 'operator',
+    E.RIGHT_OPERAND AS 'right_operand',
+    (
+        CASE
+            WHEN E.OPERATOR = '<' AND V1.VALUE < V2.VALUE THEN 'true'
+            WHEN E.OPERATOR = '=' AND V1.VALUE = V2.VALUE THEN 'true'
+            WHEN E.OPERATOR = '>' AND V1.VALUE > V2.VALUE THEN 'true'
+            ELSE 'false'
+        END
+    ) AS 'value'
+FROM
+    EXPRESSIONS E
+JOIN
+    VARIABLES V1
+ON 
+    E.LEFT_OPERAND = V1.NAME
+JOIN
+    VARIABLES V2
+ON
+    E.RIGHT_OPERAND = V2.NAME;

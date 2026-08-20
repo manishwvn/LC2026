daebbfb88ca217615caed8f0f5cec8db3590c2bf
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:02:54 -0500

    Create 1500-CountLargestGroup

---
+++ 1500-CountLargestGroup/1500-CountLargestGroup.py
@@ -0,0 +1,33 @@
+# Last updated: 8/20/2026, 2:02:54 AM
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+
+        def digit_sum(num):
+            sum_ = 0
+            while num:
+                sum_ += num % 10
+                num //= 10
+
+            return sum_
+
+        hm = {}
+        max_val = 0
+        for i in range(1,n+1):
+            key = digit_sum(i)
+
+            if key in hm:
+                hm[key] += 1
+            else:
+                hm[key] = 1
+            
+            max_val = max(max_val, hm[key])
+        
+        print(max_val)
+        res = 0
+
+        for val in hm.values():
+            if val == max_val:
+                res += 1
+
+        return res
+        

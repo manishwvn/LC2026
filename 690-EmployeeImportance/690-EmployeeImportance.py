720793bd06f9573d4a7544ab6123ce12259784c2
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:09:50 -0500

    Create 690-EmployeeImportance

---
+++ 690-EmployeeImportance/690-EmployeeImportance.py
@@ -0,0 +1,34 @@
+# Last updated: 8/20/2026, 2:09:49 AM
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        
+        hm = {}
+        
+        for emp in employees:
+            hm[emp.id] = emp
+            
+        queue = deque()
+        queue.append(id)
+        result = 0
+        while queue:
+            eid = queue.popleft()
+            emp = hm[eid]
+            result += emp.importance
+            for sub in emp.subordinates:
+                queue.append(sub)
+                
+        return result
+            
+            
+            
+        
+        

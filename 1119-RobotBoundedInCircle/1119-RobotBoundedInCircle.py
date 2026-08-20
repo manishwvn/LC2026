a35de406e44025baed8ab796fcdf8d9215d17ef6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:06:22 -0500

    Create 1119-RobotBoundedInCircle

---
+++ 1119-RobotBoundedInCircle/1119-RobotBoundedInCircle.py
@@ -0,0 +1,20 @@
+# Last updated: 8/20/2026, 2:06:22 AM
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+
+        start = [0, 0]
+        dirs = [0, 1]
+
+        for ins in instructions:
+            if ins == "L":
+                dirs = [-dirs[1], dirs[0]]
+            elif ins == "R":
+                dirs = [dirs[1], -dirs[0]]
+            else:
+                start[0] += dirs[0]
+                start[1] += dirs[1]
+
+        return start == [0, 0] or dirs != [0, 1]
+
+        
+        

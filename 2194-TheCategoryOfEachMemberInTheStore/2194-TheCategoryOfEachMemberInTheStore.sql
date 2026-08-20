cf6fb542e9a81bd92f9df65badb551f6aaed0e86
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:58:27 -0500

    Create 2194-TheCategoryOfEachMemberInTheStore

---
+++ 2194-TheCategoryOfEachMemberInTheStore/2194-TheCategoryOfEachMemberInTheStore.sql
@@ -0,0 +1,35 @@
+-- Last updated: 8/20/2026, 1:58:27 AM
+with cte as (
+    select
+        m.member_id,
+        m.name,
+        count(v.visit_id) as visit_counts,
+        count(p.visit_id) as purchase_counts
+    from
+        members m
+    left join
+        visits v
+    on
+        m.member_id = v.member_id
+    left join
+        purchases p
+    on
+        p.visit_id = v.visit_id
+    group by
+        1, 2)
+
+select
+    member_id,
+    name,
+    case
+        when
+            visit_counts = 0 then 'Bronze'
+        when
+            (purchase_counts  * 100) / visit_counts < 50 then 'Silver'
+        when
+            (purchase_counts  * 100) / visit_counts >= 50 and (purchase_counts  * 100) / visit_counts < 80 then 'Gold'
+        else
+            'Diamond'
+    end as category
+from
+    cte;

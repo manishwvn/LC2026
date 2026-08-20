562ad07258efa62864172fcddcebd9cc82a672a5
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:02:15 -0500

    Create 1582-DesignBrowserHistory

---
+++ 1582-DesignBrowserHistory/1582-DesignBrowserHistory.py
@@ -0,0 +1,44 @@
+# Last updated: 8/20/2026, 2:02:14 AM
+class Node:
+    
+    def __init__(self, url):
+        self.val = url
+        self.prev = None
+        self.next = None
+
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.start = Node(homepage)
+        
+
+    def visit(self, url: str) -> None:
+        node = Node(url)
+        self.start.next = node
+        node.prev = self.start
+        self.start = self.start.next
+        
+
+    def back(self, steps: int) -> str:
+        while steps and self.start.prev:
+            self.start = self.start.prev
+            steps -= 1
+            
+        return self.start.val
+        
+
+    def forward(self, steps: int) -> str:
+        while steps and self.start.next:
+            self.start = self.start.next
+            steps -=1
+            
+        return self.start.val
+        
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)

f27e8176f012caf872884dd6071fc584912af6fa
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:20:20 -0500

    Create 16-3sumClosest

---
+++ 16-3sumClosest/16-3sumClosest.py
@@ -0,0 +1,35 @@
+# Last updated: 8/20/2026, 2:20:19 AM
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+
+        
+        nums.sort()
+        min_diff = float('inf')
+        
+        for i in range(len(nums)):
+            a = nums[i]
+            l, r = i+1, len(nums) - 1
+
+            while l < r:
+                b, c = nums[l], nums[r]
+                total = a + b + c
+
+                if total == target:
+                    return total
+                
+                if abs(target - total) < abs(min_diff):
+                    min_diff = target - total
+                
+                
+                
+                if total < target:
+                    l += 1
+                    
+                else:
+                    r -= 1
+                
+                    
+        return target - min_diff
+                
+
+

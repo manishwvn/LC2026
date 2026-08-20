7ce6948bb075f49e9bd48bc1792573637539ad06
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:14:08 -0500

    Create 325-MaximumSizeSubarraySumEqualsK

---
+++ 325-MaximumSizeSubarraySumEqualsK/325-MaximumSizeSubarraySumEqualsK.py
@@ -0,0 +1,25 @@
+# Last updated: 8/20/2026, 2:14:08 AM
+class Solution:
+    def maxSubArrayLen(self, nums: List[int], k: int) -> int:
+        
+        prefix = 0
+        hm = {}
+        result = 0
+        
+        for i, num in enumerate(nums):
+            prefix += num
+            
+            if prefix == k:
+                result = i + 1
+                
+            if prefix - k in hm:
+                result = max(result, i - hm[prefix - k])
+                
+            if prefix not in hm:
+                hm[prefix] = i
+          
+        return result
+            
+            
+        
+        

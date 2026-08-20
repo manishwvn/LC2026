3d260e47812177551a35f8402fd89a532377bd73
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:05:34 -0500

    Create 1195-DistributeCandiesToPeople

---
+++ 1195-DistributeCandiesToPeople/1195-DistributeCandiesToPeople.py
@@ -0,0 +1,13 @@
+# Last updated: 8/20/2026, 2:05:34 AM
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        res = [0] * num_people
+        i = 1
+
+        while candies > 0:
+            give = min(i, candies)
+            res[(i - 1) % num_people] += give
+            candies -= give
+            i += 1
+
+        return res

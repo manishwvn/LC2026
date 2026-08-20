aab655b698bba322cc42344abfda1c026475ba28
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:02:00 -0500

    Create 1639-FriendlyMoviesStreamedLastMonth

---
+++ 1639-FriendlyMoviesStreamedLastMonth/1639-FriendlyMoviesStreamedLastMonth.sql
@@ -0,0 +1,17 @@
+-- Last updated: 8/20/2026, 2:01:59 AM
+select
+    distinct c.title as TITLE
+from
+    tvprogram t
+join
+    content c
+on
+    t.content_id = c.content_id
+where
+    month(t.program_date) = 6 
+    and 
+    year(t.program_date) = 2020
+    and
+    c.kids_content = 'Y'
+    and
+    c.content_type = 'Movies';

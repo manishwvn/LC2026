ad484ef62d38aa3f2c9880a9b296271c4d8c5b3c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:56:00 -0500

    Create 2692-TakeGiftsFromTheRichestPile

---
+++ 2692-TakeGiftsFromTheRichestPile/2692-TakeGiftsFromTheRichestPile.py
@@ -0,0 +1,18 @@
+# Last updated: 8/20/2026, 1:55:59 AM
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+
+        heap = []
+        for gift in gifts:
+            heap.append(-gift)
+        heapify(heap)
+
+        for _ in range(k):
+            gift = -heappop(heap)
+            heappush(heap, -int(math.sqrt(gift)))
+        
+        sum_ = 0
+        for i in range(len(heap)):
+            sum_ += (-heap[i])
+        return sum_
+        

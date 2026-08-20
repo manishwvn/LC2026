b53e07a2b3430e8e84fbfef5f79b62f2fb1718b5
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:05:21 -0500

    Create 1221-ElementAppearingMoreThan25InSortedArray

---
+++ 1221-ElementAppearingMoreThan25InSortedArray/1221-ElementAppearingMoreThan25InSortedArray.py
@@ -0,0 +1,18 @@
+# Last updated: 8/20/2026, 2:05:21 AM
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        n = len(arr)
+        if n == 1:
+            return arr[0]
+ 
+        count = 1
+        for i in range(1,n):
+            if arr[i-1] == arr[i]:
+                count += 1
+            else:
+                if count > n / 4:
+                    return arr[i-1]
+                count = 1
+
+        if count > n / 4:
+            return arr[-1]        

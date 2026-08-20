6d4a92c2b511d85bba5bcf3b2560629352857df5
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:06:30 -0500

    Create 1092-MaximumDifferenceBetweenNodeAndAncestor

---
+++ 1092-MaximumDifferenceBetweenNodeAndAncestor/1092-MaximumDifferenceBetweenNodeAndAncestor.py
@@ -0,0 +1,35 @@
+# Last updated: 8/20/2026, 2:06:30 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        if not root: 
+            return 0
+        
+        global result 
+        result = 0
+        minm, maxm = 10**6, -1
+        
+        def helper(root, minm, maxm):
+            global result
+
+            if not root:
+                # result = max(result, abs(maxm - minm))
+                return
+            
+            minm = min(minm, root.val)
+            maxm = max(maxm, root.val)
+            result = max(result, maxm - minm)
+
+            helper(root.left, minm , maxm)
+            helper(root.right, minm, maxm)        
+            
+            
+        helper(root, minm, maxm)
+        return result
+        
+        

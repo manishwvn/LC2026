4e8b64779e004d9499b8e3b4aab7e7d0549b68d4
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:05:13 -0500

    Create 1231-ReplaceElementsWithGreatestElementOnRightSide

---
+++ 1231-ReplaceElementsWithGreatestElementOnRightSide/1231-ReplaceElementsWithGreatestElementOnRightSide.py
@@ -0,0 +1,13 @@
+# Last updated: 8/20/2026, 2:05:13 AM
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+
+        result = [-1] * len(arr)
+        largest = arr[-1]
+
+        for i in range(len(arr)-2, -1, -1):
+            result[i] = largest
+            largest = max(largest, arr[i])
+
+        return result 
+        

a44dd8e350f241aaf62db929152378c7459c7e70
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:02:09 -0500

    Create 1618-DeleteNNodesAfterMNodesOfALinkedList

---
+++ 1618-DeleteNNodesAfterMNodesOfALinkedList/1618-DeleteNNodesAfterMNodesOfALinkedList.py
@@ -0,0 +1,28 @@
+# Last updated: 8/20/2026, 2:02:09 AM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteNodes(self, head: Optional[ListNode], m: int, n: int) -> Optional[ListNode]:
+
+        curr = head
+        last = None
+
+        while curr:
+            m_count, n_count = m, n
+
+            while curr and m_count:
+                last = curr
+                curr = curr.next
+                m_count -= 1
+
+            while curr and n_count:
+                curr = curr.next
+                n_count -= 1
+
+            last.next = curr
+        
+        return head
+

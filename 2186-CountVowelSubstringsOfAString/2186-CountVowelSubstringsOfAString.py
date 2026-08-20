649c281ade0c011335570d54de606dc0a367d95e
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:58:30 -0500

    Create 2186-CountVowelSubstringsOfAString

---
+++ 2186-CountVowelSubstringsOfAString/2186-CountVowelSubstringsOfAString.py
@@ -0,0 +1,14 @@
+# Last updated: 8/20/2026, 1:58:30 AM
+class Solution:
+    def countVowelSubstrings(self, word: str) -> int:
+        
+        v={'a','e','i','o','u'}
+        c=0
+        
+        for i in range(len(word)-4):
+            for j in range(i+5,len(word)+1):
+                if set(word[i:j])==v:
+                    c+=1
+        return c
+            
+        

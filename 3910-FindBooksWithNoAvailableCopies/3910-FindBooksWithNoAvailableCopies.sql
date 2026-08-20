317fa812553134a0ca6a38968966033cf81fe097
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:52:44 -0500

    Create 3910-FindBooksWithNoAvailableCopies

---
+++ 3910-FindBooksWithNoAvailableCopies/3910-FindBooksWithNoAvailableCopies.sql
@@ -0,0 +1,31 @@
+-- Last updated: 8/20/2026, 1:52:43 AM
+with borrowed_counts as (
+    select
+        book_id,
+        count(*) as borrowed_copies
+    from
+        borrowing_records
+    where
+        return_date is NULL
+    group by
+        book_id
+)
+
+select
+    l.book_id,
+    l.title,
+    l.author,
+    l.genre,
+    l.publication_year,
+    b.borrowed_copies as current_borrowers
+from
+    library_books l 
+join
+    borrowed_counts b
+on
+    l.book_id = b.book_id
+where
+    l.total_copies = b.borrowed_copies
+order by
+    current_borrowers DESC,
+    l.title ASC;

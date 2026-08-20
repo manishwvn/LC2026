bd0e80a5e5d322efd1375c2dfa05d94f48b0f70b
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:54:55 -0500

    Create 3283-FindThirdTransaction

---
+++ 3283-FindThirdTransaction/3283-FindThirdTransaction.sql
@@ -0,0 +1,27 @@
+-- Last updated: 8/20/2026, 1:54:54 AM
+with cte as (select
+    *,
+    rank() over(partition by user_id order by transaction_date) as trans_rnk
+from
+    transactions),
+
+cte2 as (
+    select
+        *,
+        rank() over(partition by user_id order by spend) as spend_rnk
+    from
+        cte
+    where
+        trans_rnk <= 3
+)
+
+select
+    user_id, 
+    spend as third_transaction_spend, 
+    transaction_date as third_transaction_date
+from
+    cte2
+where
+    trans_rnk = 3
+    and
+    spend_rnk = 3;

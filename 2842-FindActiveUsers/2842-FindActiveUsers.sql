bc8ee4f8d9d72e98e8d5f48679ec11525001c042
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:55:44 -0500

    Create 2842-FindActiveUsers

---
+++ 2842-FindActiveUsers/2842-FindActiveUsers.sql
@@ -0,0 +1,10 @@
+-- Last updated: 8/20/2026, 1:55:44 AM
+with cte as (
+    select
+        *,
+        count(user_id) over(partition by user_id order by created_at range between interval 7 day preceding and current row) as roll_cnt
+    from
+        users
+)
+
+select distinct user_id from cte where roll_cnt > 1;

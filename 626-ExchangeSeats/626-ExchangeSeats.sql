e8962b967ebc4a93c038394fca97eb962921708b
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:10:30 -0500

    Create 626-ExchangeSeats

---
+++ 626-ExchangeSeats/626-ExchangeSeats.sql
@@ -0,0 +1,25 @@
+-- Last updated: 8/20/2026, 2:10:30 AM
+-- if id is even then decrease by 1
+-- if id is odd 
+    -- and id = max then keep as it is
+    -- else:
+    --     id + 1
+
+with cte2 as (
+    select
+        *,
+        max(id) over() as max_id
+    from
+        seat
+)
+select
+    case
+        when id % 2 = 0 then id - 1
+        when id % 2 = 1 and id = max_id then id
+        else id + 1
+    end as id,
+    student
+from
+    cte2
+order by
+    1;

17fb72a9adfdb8256cdf045185b6a4a8a954f1f2
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:16:10 -0500

    Create 197-RisingTemperature

---
+++ 197-RisingTemperature/197-RisingTemperature.sql
@@ -0,0 +1,44 @@
+-- Last updated: 8/20/2026, 2:16:10 AM
+-- -- select
+-- --     w1.id as Id
+-- -- from
+-- --     Weather w1
+-- -- left join
+-- --     Weather w2
+-- -- on
+-- --     w2.recordDate = w1.recordDate - INTERVAL 1 DAY
+-- -- where
+-- --     w1.temperature > w2.temperature;
+-- with cte as (
+-- select
+--     *,
+--     LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date,
+--     lag(temperature) over(order by recorddate) as prev
+-- from
+--     weather)
+
+-- select
+--     id
+-- from
+--     cte
+-- where
+--     temperature > prev
+--     and
+--     recorddate = date_add(prev_date, interval 1 day)
+
+with cte as (
+select
+    *,
+    lag(recordDate) over(order by recordDate) as prev_date,
+    lag(temperature) over(order by recordDate) as prev_temp
+from
+    weather)
+
+select
+    id
+from
+    cte
+where
+    prev_date = recordDate - interval 1 day
+    and
+    temperature > prev_temp;

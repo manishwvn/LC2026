66abcce33c1ab4b795076847bf09910d395fc0b6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:12:45 -0500

    Create 441-ArrangingCoins

---
+++ 441-ArrangingCoins/441-ArrangingCoins.py
@@ -0,0 +1,19 @@
+# Last updated: 8/20/2026, 2:12:45 AM
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+
+        l, r = 1, n
+
+        while l <= r:
+            mid = (l + r) // 2
+            coins = (mid * (mid+1)) // 2
+            if coins == n:
+                return mid
+            elif coins > n:
+                r = mid - 1
+
+            else:
+                l = mid + 1
+
+        return r
+        

95bb7f2acdcdfe37b37a54c29911b767100dbdaf
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:15:32 -0500

    Create 222-CountCompleteTreeNodes

---
+++ 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.py
@@ -0,0 +1,58 @@
+# Last updated: 8/20/2026, 2:15:32 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        
+        if not root: return 0
+        
+        def depth(root):
+            
+            d = 0
+            
+            while root.left:
+                d += 1
+                root = root.left
+                
+            return d
+        
+        def check(idx, d, node):
+            left, right = 0, 2**d - 1
+            
+            for _ in range(d):
+                mid = (left + right) // 2
+                
+                if idx <= mid:
+                    node = node.left
+                    right = mid
+                    
+                else:
+                    node = node.right
+                    left = mid + 1
+                    
+            return node 
+        
+        
+        d = depth(root)
+        if d == 0: return 1
+        
+        left, right = 1, 2**d - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if check(mid, d, root):
+                left = mid + 1
+                
+            else:
+                right = mid - 1
+                
+        return (2 ** d - 1) + left
+        
+                
+        
+        

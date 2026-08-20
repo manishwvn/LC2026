dc504e168f03c437755010dd021e7166df6812a6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:01:02 -0500

    Create 1773-PercentageOfUsersAttendedAContest

---
+++ 1773-PercentageOfUsersAttendedAContest/1773-PercentageOfUsersAttendedAContest.sql
@@ -0,0 +1,13 @@
+-- Last updated: 8/20/2026, 2:01:02 AM
+select
+    r.contest_id,
+    round(count(distinct r.user_id) / count(distinct u.user_id) * 100, 2)
+    as percentage
+from
+    users u
+cross join
+    register r
+group by
+    r.contest_id
+order by
+    percentage desc, r.contest_id

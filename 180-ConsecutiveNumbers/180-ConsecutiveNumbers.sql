b908547ce256296d8616a60fe9bd65be0ab1ac41
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:16:22 -0500

    Create 180-ConsecutiveNumbers

---
+++ 180-ConsecutiveNumbers/180-ConsecutiveNumbers.sql
@@ -0,0 +1,21 @@
+-- Last updated: 8/20/2026, 2:16:22 AM
+with cte as (select
+    *,
+    row_number() over(partition by num order by id) as rnk
+from
+    logs),
+
+ cte2 as (select
+    *,
+    id - rnk as grp
+from
+    cte)
+
+select
+    distinct num as ConsecutiveNums
+from
+    cte2
+group by
+    num, grp
+having
+    count(id) >= 3;

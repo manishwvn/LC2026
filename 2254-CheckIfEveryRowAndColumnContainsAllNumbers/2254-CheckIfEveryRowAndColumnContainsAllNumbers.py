0a34da069462f3e6a5337d0c928b0f81070316b9
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:58:02 -0500

    Create 2254-CheckIfEveryRowAndColumnContainsAllNumbers

---
+++ 2254-CheckIfEveryRowAndColumnContainsAllNumbers/2254-CheckIfEveryRowAndColumnContainsAllNumbers.py
@@ -0,0 +1,15 @@
+# Last updated: 8/20/2026, 1:58:01 AM
+class Solution:
+    def checkValid(self, matrix: List[List[int]]) -> bool:
+        
+        n = len(matrix)
+        rows, cols = defaultdict(set), defaultdict(set)
+        
+        for i in range(n):
+            for j in range(n):
+                if matrix[i][j] in rows[i] or matrix[i][j] in cols[j]:
+                    return False
+                rows[i].add(matrix[i][j])
+                cols[j].add(matrix[i][j])
+                
+        return True

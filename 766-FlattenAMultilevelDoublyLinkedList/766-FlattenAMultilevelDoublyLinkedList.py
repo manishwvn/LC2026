4d2f0616fab470884807ac40c354b9aac2a702c0
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:09:13 -0500

    Create 766-FlattenAMultilevelDoublyLinkedList

---
+++ 766-FlattenAMultilevelDoublyLinkedList/766-FlattenAMultilevelDoublyLinkedList.py
@@ -0,0 +1,38 @@
+# Last updated: 8/20/2026, 2:09:13 AM
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        
+        if not head:
+            return None
+        
+        stack = []
+        dummy = head
+        
+        while head:
+            if head.child:
+                if head.next:
+                    stack.append(head.next)
+                head.next = head.child
+                head.next.prev = head
+                head.child = None
+                
+            if not head.next and stack:
+                head.next = stack.pop()
+                head.next.prev = head
+            
+            head = head.next
+            
+        return dummy
+            
+              
+        

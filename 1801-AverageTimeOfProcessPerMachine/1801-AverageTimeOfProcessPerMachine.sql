3832e402c8a4f1837fb992f8998950b097203b84
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:00:49 -0500

    Create 1801-AverageTimeOfProcessPerMachine

---
+++ 1801-AverageTimeOfProcessPerMachine/1801-AverageTimeOfProcessPerMachine.sql
@@ -0,0 +1,11 @@
+-- Last updated: 8/20/2026, 2:00:49 AM
+
+
+SELECT a1.machine_id , ROUND(SUM(a2.timestamp - a1.timestamp)/COUNT(*), 3) as processing_time
+FROM Activity a1
+INNER JOIN Activity a2 
+ON a1.machine_id = a2.machine_id
+AND a1.process_id = a2.process_id
+AND a1.activity_type ='start'
+AND a2.activity_type ='end'  
+GROUP BY a1.machine_id;

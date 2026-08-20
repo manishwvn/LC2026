55c7ad675ca0d4c43428a708fda9fc6ddc1f7fa0
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:13:24 -0500

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.py
@@ -0,0 +1,12 @@
+# Last updated: 8/20/2026, 2:13:23 AM
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hm = Counter(magazine)
+        for char in ransomNote:
+            if char in hm and hm[char] != 0:
+                hm[char] -=1
+            else:
+                return False
+        return True
+        

dacc1059ecebdf527aacdcf2a83d4bfa83639898
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:02:59 -0500

    Create 1495-NumberOfTrustedContactsOfACustomer

---
+++ 1495-NumberOfTrustedContactsOfACustomer/1495-NumberOfTrustedContactsOfACustomer.sql
@@ -0,0 +1,14 @@
+-- Last updated: 8/20/2026, 2:02:59 AM
+# Write your MySQL query statement below
+SELECT 
+    i.invoice_id, 
+    c.customer_name, 
+    i.price, 
+    COUNT(co.contact_name) contacts_cnt,
+    COUNT(c2.email) trusted_contacts_cnt
+FROM Invoices i
+JOIN Customers c ON i.user_id = c.customer_id
+LEFT JOIN Contacts co ON c.customer_id = co.user_id
+LEFT JOIN Customers c2 ON co.contact_email = c2.email
+GROUP BY 1
+ORDER BY 1

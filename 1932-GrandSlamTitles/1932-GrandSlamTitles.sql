133fe624de73299aa70f0eae429c69d67ac51f5e
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:59:56 -0500

    Create 1932-GrandSlamTitles

---
+++ 1932-GrandSlamTitles/1932-GrandSlamTitles.sql
@@ -0,0 +1,18 @@
+-- Last updated: 8/20/2026, 1:59:56 AM
+# Write your MySQL query statement below
+
+with cte as (
+select Wimbledon as player_id from Championships
+union all
+select Fr_open as player_id from Championships
+union all
+select US_open as player_id from Championships
+union all
+select Au_open as player_id from Championships
+)
+
+select player_id, player_name, count(player_id) as grand_slams_count
+from
+cte
+join Players using(player_id)
+group by player_id, player_name

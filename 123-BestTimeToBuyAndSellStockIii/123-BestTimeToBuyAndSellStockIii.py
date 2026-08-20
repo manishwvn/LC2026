1031ba34d15fe21a804b7d8331f514a447f64750
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:17:30 -0500

    Create 123-BestTimeToBuyAndSellStockIii

---
+++ 123-BestTimeToBuyAndSellStockIii/123-BestTimeToBuyAndSellStockIii.py
@@ -0,0 +1,15 @@
+# Last updated: 8/20/2026, 2:17:30 AM
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        buy1, buy2 = float('inf'), float('inf')
+        sell1, sell2 = 0, 0
+
+        for price in prices:
+            buy1 = min(buy1, price)
+            sell1 = max(sell1, price - buy1)
+            buy2 = min(buy2, price - sell1)
+            sell2 = max(sell2, price - buy2)
+
+        return sell2
+        

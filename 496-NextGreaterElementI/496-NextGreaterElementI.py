83e92e261f50dcbe71679c06c556ad2791f44735
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:12:18 -0500

    Create 496-NextGreaterElementI

---
+++ 496-NextGreaterElementI/496-NextGreaterElementI.py
@@ -0,0 +1,20 @@
+# Last updated: 8/20/2026, 2:12:18 AM
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        res = [-1] * len(nums1)
+        hm = {n : i for i, n in enumerate(nums1)}
+        
+        stack = []
+        
+        for i in range(len(nums2)):
+            while stack and nums2[i] > stack[-1]:
+                value = stack.pop()
+                idx = hm[value]
+                res[idx] = nums2[i]
+                
+            if nums2[i] in hm:
+                stack.append(nums2[i])
+            
+        return res
+        

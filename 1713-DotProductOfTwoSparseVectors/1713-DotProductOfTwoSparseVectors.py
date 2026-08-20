9440d162502992c2f47d5ff2b8d229e96f9c8ee2
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:01:28 -0500

    Create 1713-DotProductOfTwoSparseVectors

---
+++ 1713-DotProductOfTwoSparseVectors/1713-DotProductOfTwoSparseVectors.py
@@ -0,0 +1,23 @@
+# Last updated: 8/20/2026, 2:01:28 AM
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.hm = {}
+        for i, val in enumerate(nums):
+            if val:
+                self.hm[i] = val
+        
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        result = 0
+        for i, val in self.hm.items():
+            if i in vec.hm:
+                result += self.hm[i] * vec.hm[i]
+                
+        return result
+        
+
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)

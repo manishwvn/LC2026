fc2d6b17f365625b48f4ff6e2b766dbbdd36b37b
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:56:12 -0500

    Create 2580-CircularSentence

---
+++ 2580-CircularSentence/2580-CircularSentence.py
@@ -0,0 +1,19 @@
+# Last updated: 8/20/2026, 1:56:12 AM
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+
+        words = sentence.split()
+
+        if len(words) == 1:
+            return True if words[0][0] == words[0][-1] else False
+
+        for i in range(1, len(words)):
+            if (words[i][0]) != words[i-1][-1]:
+                return False
+
+        if words[-1][-1] != words[0][0]:
+            return False
+        
+        return True
+
+        

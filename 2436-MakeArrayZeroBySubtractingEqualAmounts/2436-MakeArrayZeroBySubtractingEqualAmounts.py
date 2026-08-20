1b8e743c462596f4327ba1e27ca785cd547e472f
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:57:01 -0500

    Create 2436-MakeArrayZeroBySubtractingEqualAmounts

---
+++ 2436-MakeArrayZeroBySubtractingEqualAmounts/2436-MakeArrayZeroBySubtractingEqualAmounts.py
@@ -0,0 +1,16 @@
+# Last updated: 8/20/2026, 1:57:00 AM
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return 0 if nums[0] == 0 else 1
+
+        ops = set()
+        for num in nums:
+            if num != 0:
+                ops.add(num)
+        return len(ops)
+
+
+        
+        

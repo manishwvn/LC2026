bebd3c7407ca6b63eb287e610213db22c3ed342c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:11:54 -0500

    Create 528-SwappingNodesInALinkedList

---
+++ 528-SwappingNodesInALinkedList/528-SwappingNodesInALinkedList.py
@@ -0,0 +1,29 @@
+# Last updated: 8/20/2026, 2:11:54 AM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+        curr = head
+        count = 0
+        left, right = None, head
+        while curr:
+            count += 1
+
+            if count == k:
+                left = curr
+                
+            if count > k:
+                right = right.next
+            
+            curr = curr.next
+
+        left.val, right.val = right.val, left.val
+
+        return head
+                
+
+        

50d4f5149c490cce927120c108fe1a0d6d8d5218
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:19:29 -0500

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.py
@@ -0,0 +1,22 @@
+# Last updated: 8/20/2026, 2:19:28 AM
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        def backtrack(i):
+
+            if i == len(nums):
+                result.append(nums.copy())
+                return
+
+            for j in range(i, len(nums)):
+                nums[i], nums[j] = nums[j], nums[i]
+                backtrack(i+1)
+                nums[i], nums[j] = nums[j], nums[i]
+
+        result = []
+        backtrack(0)
+        return result
+
+
+
+        

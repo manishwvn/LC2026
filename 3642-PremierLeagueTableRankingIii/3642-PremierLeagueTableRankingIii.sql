c454806e794e9d52e1785657a28dde2588428edf
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:53:31 -0500

    Create 3642-PremierLeagueTableRankingIii

---
+++ 3642-PremierLeagueTableRankingIii/3642-PremierLeagueTableRankingIii.sql
@@ -0,0 +1,20 @@
+-- Last updated: 8/20/2026, 1:53:31 AM
+select
+    season_id,
+    team_id,
+    team_name,
+    (3*wins + 1*draws) as points,
+    (goals_for - goals_against) as goal_difference,
+    row_number() over(partition by season_id order by (3*wins + 1*draws) desc,
+        (goals_for - goals_against) desc, team_name asc)
+        as position
+from
+    seasonstats
+group by
+    season_id,
+    team_id,
+    team_name
+order by
+    season_id,
+    position,
+    team_name;

1fec3461ac23814fcc8f81b2e16713c9c9240247
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:01:29 -0500

    Create 1712-UniqueOrdersAndCustomersPerMonth

---
+++ 1712-UniqueOrdersAndCustomersPerMonth/1712-UniqueOrdersAndCustomersPerMonth.sql
@@ -0,0 +1,14 @@
+-- Last updated: 8/20/2026, 2:01:29 AM
+SELECT
+    DATE_FORMAT(ORDER_DATE, '%Y-%m') as 'month',
+    COUNT(DISTINCT ORDER_ID) AS 'order_count',
+    COUNT(DISTINCT CUSTOMER_ID) AS 'customer_count'    
+FROM
+    ORDERS
+WHERE
+    INVOICE > 20
+GROUP BY
+    month;
+
+    
+    

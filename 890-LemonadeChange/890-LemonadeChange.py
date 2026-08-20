4efc8482951d4b82989845fb605586dea965f972
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:08:03 -0500

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.py
@@ -0,0 +1,28 @@
+# Last updated: 8/20/2026, 2:08:03 AM
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+
+        fives = 0
+        tens = 0
+
+        for bill in bills:
+            if bill == 5:
+                fives += 1
+            elif bill == 10:
+                if fives > 0:
+                    fives -= 1
+                    tens += 1
+                else:
+                    return False
+            else:
+                print(fives)
+                if fives > 0 and tens > 0:
+                    fives -= 1
+                    tens -= 1
+                elif fives >= 3:
+                    fives -= 3
+                else:
+                    return False
+                
+        return True
+            

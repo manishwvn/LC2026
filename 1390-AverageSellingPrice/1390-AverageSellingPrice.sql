c3bfe8ca21c724dc967c897f7452d04703894d7a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:03:57 -0500

    Create 1390-AverageSellingPrice

---
+++ 1390-AverageSellingPrice/1390-AverageSellingPrice.sql
@@ -0,0 +1,14 @@
+-- Last updated: 8/20/2026, 2:03:57 AM
+select
+    p.product_id,
+    coalesce(round(sum(p.price * u.units) / sum(u.units), 2), 0) as average_price
+from 
+    prices p 
+left join
+    unitssold u 
+on
+    p.product_id = u.product_id
+    and
+    u.purchase_date between p.start_date and p.end_date
+group by
+    p.product_id;

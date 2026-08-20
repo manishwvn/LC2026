fd37066ed6eb7621fc7280994156457d8cb9b1be
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:06:26 -0500

    Create 1102-CheckIfANumberIsMajorityElementInASortedArray

---
+++ 1102-CheckIfANumberIsMajorityElementInASortedArray/1102-CheckIfANumberIsMajorityElementInASortedArray.py
@@ -0,0 +1,22 @@
+# Last updated: 8/20/2026, 2:06:26 AM
+class Solution:
+    def isMajorityElement(self, nums: List[int], target: int) -> bool:
+
+        def find_left(l, r, nums, target):
+            while l <= r:
+                mid = (l + r) // 2
+
+                if nums[mid] >= target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+
+            return r
+
+        left = find_left(0, len(nums) -1, nums, target) + 1
+        right = left + (len(nums) // 2)
+        
+        if right < len(nums) and nums[right] == target:
+            return True
+
+        return False

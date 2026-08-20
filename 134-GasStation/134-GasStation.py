7963c6dcd6183f40278824cb3248ebbd25bc8aea
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:17:16 -0500

    Create 134-GasStation

---
+++ 134-GasStation/134-GasStation.py
@@ -0,0 +1,21 @@
+# Last updated: 8/20/2026, 2:17:15 AM
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
+        n = len(gas)
+        
+        curr, total = 0, 0 
+        start = 0
+        
+        for i in range(n):
+            total += gas[i] - cost[i]
+            curr += gas[i] - cost[i]
+            
+            if curr < 0:
+                start = i + 1
+                curr = 0
+                
+                
+        return start if total >= 0 else -1
+                
+        

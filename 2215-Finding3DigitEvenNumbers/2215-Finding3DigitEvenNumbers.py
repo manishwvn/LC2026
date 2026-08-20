6be3360e66a0e6fee41fe2fc38b858f5ec9af4ed
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:58:15 -0500

    Create 2215-Finding3DigitEvenNumbers

---
+++ 2215-Finding3DigitEvenNumbers/2215-Finding3DigitEvenNumbers.py
@@ -0,0 +1,31 @@
+# Last updated: 8/20/2026, 1:58:14 AM
+from typing import List
+
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        digit_count = [0] * 10
+        for d in digits:
+            digit_count[d] += 1
+
+        result = []
+
+        for num in range(100, 1000, 2):  # even numbers only
+            temp = [0] * 10
+            a = num // 100
+            b = (num // 10) % 10
+            c = num % 10
+            temp[a] += 1
+            temp[b] += 1
+            temp[c] += 1
+
+            # check if num can be built from digits
+            is_valid = True
+            for d in range(10):
+                if temp[d] > digit_count[d]:
+                    is_valid = False
+                    break
+
+            if is_valid:
+                result.append(num)
+
+        return sorted(result)

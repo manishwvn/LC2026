24da4fa82b2002226e7d980f23d6ef81f8e93a63
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:53:06 -0500

    Create 3790-FruitsIntoBasketsIi

---
+++ 3790-FruitsIntoBasketsIi/3790-FruitsIntoBasketsIi.py
@@ -0,0 +1,17 @@
+# Last updated: 8/20/2026, 1:53:06 AM
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        used = set()
+
+        for i in range(len(fruits)):
+            for j in range(len(baskets)):
+                if j in used:
+                    continue
+                if fruits[i] <= baskets[j]:
+                    used.add(j)
+                    break
+
+            if len(used) == len(baskets):
+                break
+
+        return len(fruits) - len(used)

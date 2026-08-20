26f475e9eb857aa4f38e46ff40ad447ee199d30d
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:56:20 -0500

    Create 2551-ApplyOperationsToAnArray

---
+++ 2551-ApplyOperationsToAnArray/2551-ApplyOperationsToAnArray.py
@@ -0,0 +1,14 @@
+# Last updated: 8/20/2026, 1:56:19 AM
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        j = 0
+        for i in range(len(nums)):
+            if i < len(nums) - 1 and nums[i] == nums[i+1]:
+                nums[i] = nums[i] * 2
+                nums[i+1] = 0
+
+            if nums[i]:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+
+        return nums

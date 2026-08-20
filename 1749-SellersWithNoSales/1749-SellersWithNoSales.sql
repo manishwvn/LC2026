0d7cf70bc04f88449da4326aca67fc95751a2947
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:01:12 -0500

    Create 1749-SellersWithNoSales

---
+++ 1749-SellersWithNoSales/1749-SellersWithNoSales.sql
@@ -0,0 +1,21 @@
+-- Last updated: 8/20/2026, 2:01:12 AM
+with cte as (
+    select
+        *
+    from
+        orders
+    where
+        year(sale_date) = 2020)
+
+select
+    s.seller_name
+from
+    seller s
+left join
+    cte c
+on
+    s.seller_id = c.seller_id
+where
+    c.order_id is null
+order by
+    s.seller_name;

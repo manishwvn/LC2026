23adf036fb59bcadc57fe8f5c0818280380dbc7e
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:57:21 -0500

    Create 2376-NumberOfTimesADriverWasAPassenger

---
+++ 2376-NumberOfTimesADriverWasAPassenger/2376-NumberOfTimesADriverWasAPassenger.sql
@@ -0,0 +1,12 @@
+-- Last updated: 8/20/2026, 1:57:21 AM
+select
+    r1.driver_id,
+    count(distinct r2.ride_id) as cnt
+from
+    rides r1
+left join
+    rides r2
+on
+    r1.driver_id = r2.passenger_id
+group by
+    1;

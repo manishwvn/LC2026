33fa9bfa49a9c4b1bad9defac771cd002e16a76f
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:56:52 -0500

    Create 2452-ProductSalesAnalysisV

---
+++ 2452-ProductSalesAnalysisV/2452-ProductSalesAnalysisV.sql
@@ -0,0 +1,14 @@
+-- Last updated: 8/20/2026, 1:56:52 AM
+select
+    s.user_id,
+    sum(p.price * s.quantity) as spending
+from
+    sales s
+join
+    product p
+on
+    s.product_id = p.product_id
+group by
+    s.user_id
+order by
+    2 desc, 1 asc;

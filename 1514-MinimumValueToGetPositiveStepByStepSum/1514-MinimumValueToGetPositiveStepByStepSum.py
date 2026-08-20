8640f8180345dbc0b9b0190c3015262cf04ad4ac
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:02:46 -0500

    Create 1514-MinimumValueToGetPositiveStepByStepSum

---
+++ 1514-MinimumValueToGetPositiveStepByStepSum/1514-MinimumValueToGetPositiveStepByStepSum.py
@@ -0,0 +1,15 @@
+# Last updated: 8/20/2026, 2:02:46 AM
+class Solution:
+    def minStartValue(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return abs(nums[0]) + 1 if nums[0] < 1 else 1
+
+        prefix_sum = 0
+        min_val = float("inf")
+        for num in nums:
+            prefix_sum += num
+            min_val = min(min_val, prefix_sum)
+
+        return abs(min_val) + 1 if min_val < 0 else 1
+        

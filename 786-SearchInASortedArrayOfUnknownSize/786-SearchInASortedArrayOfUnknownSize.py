01170af8ab8ec7df361916b064461b96d52eb726
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:09:02 -0500

    Create 786-SearchInASortedArrayOfUnknownSize

---
+++ 786-SearchInASortedArrayOfUnknownSize/786-SearchInASortedArrayOfUnknownSize.py
@@ -0,0 +1,31 @@
+# Last updated: 8/20/2026, 2:09:02 AM
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader:
+#    def get(self, index: int) -> int:
+
+class Solution:
+    def search(self, reader: 'ArrayReader', target: int) -> int:
+        
+        low, high = 0, 1
+        
+        while reader.get(high) < target:
+            low = high
+            high = 2 * high
+            
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if reader.get(mid) == target: return mid
+            
+            elif reader.get(mid) > target:
+                high = mid - 1
+                
+            else:
+                low = mid + 1
+                
+        return -1
+        
+        

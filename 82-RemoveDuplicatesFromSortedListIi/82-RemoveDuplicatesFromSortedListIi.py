95205ad9d4729d8f4becbd10068ef0553878d547
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:18:29 -0500

    Create 82-RemoveDuplicatesFromSortedListIi

---
+++ 82-RemoveDuplicatesFromSortedListIi/82-RemoveDuplicatesFromSortedListIi.py
@@ -0,0 +1,35 @@
+# Last updated: 8/20/2026, 2:18:29 AM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        
+        if not head: return None
+        
+        new_head = ListNode(-1)
+        new_head.next = head
+        prev = new_head
+        curr = head
+        
+        while curr:
+            
+            if curr.next and curr.val == curr.next.val:
+                
+                while curr.next and curr.val == curr.next.val:
+                    curr = curr.next
+                
+                prev.next = curr.next
+            
+            else:
+                prev = prev.next
+                
+            curr = curr.next
+            
+        return new_head.next
+        
+        
+        

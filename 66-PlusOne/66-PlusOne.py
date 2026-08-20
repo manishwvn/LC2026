90e3b3a8784d2b6e69ef4e287768fbae3856dc76
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:18:56 -0500

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.py
@@ -0,0 +1,14 @@
+# Last updated: 8/20/2026, 2:18:56 AM
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        carry = 1
+
+        for i in range(len(digits)-1, -1, -1):
+            num = digits[i] + carry
+            digits[i] = num % 10
+            carry = num // 10
+
+            if not carry:
+                return digits
+        return [1] + digits

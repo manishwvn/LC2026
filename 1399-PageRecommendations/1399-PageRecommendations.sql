8a8e144313388b8d5076e4f886ad2066017cf7a6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:03:53 -0500

    Create 1399-PageRecommendations

---
+++ 1399-PageRecommendations/1399-PageRecommendations.sql
@@ -0,0 +1,19 @@
+-- Last updated: 8/20/2026, 2:03:53 AM
+select
+    distinct l.page_id as recommended_page
+from
+    likes l 
+join
+    friendship f
+on
+    (l.user_id = f.user1_id and f.user2_id = 1)
+    or
+    (l.user_id = f.user2_id and f.user1_id = 1)
+left join
+    likes l1
+on
+    l1.page_id = l.page_id
+    and
+    l1.user_id = 1
+where
+    l1.user_id is null

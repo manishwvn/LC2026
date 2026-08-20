81131b9a67e1f5c8640394dfb618e1f118558dac
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:55:32 -0500

    Create 2998-CountSymmetricIntegers

---
+++ 2998-CountSymmetricIntegers/2998-CountSymmetricIntegers.py
@@ -0,0 +1,17 @@
+# Last updated: 8/20/2026, 1:55:32 AM
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+
+        res = 0
+
+        for num in range(low, high+1):
+            if num < 100 and num % 11 == 0:
+                res += 1
+
+            if 1000 <= num < 10000:
+                left = num // 1000 + num % 1000 // 100
+                right = num % 100 // 10 + num % 10
+                if left == right:
+                    res += 1
+        return res
+        

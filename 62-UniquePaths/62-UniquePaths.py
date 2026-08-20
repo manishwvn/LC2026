cf97d1f56e5e60200fe2b01b63aaee7a12919ded
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:19:03 -0500

    Create 62-UniquePaths

---
+++ 62-UniquePaths/62-UniquePaths.py
@@ -0,0 +1,16 @@
+# Last updated: 8/20/2026, 2:19:03 AM
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        cache = [1 for j in range(n)]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                cache[j] = cache[j] + cache[j-1]
+        
+        print(cache)
+
+        return cache[-1]
+
+
+        

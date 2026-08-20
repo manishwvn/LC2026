7247d95a896af0d29450ba13033467e042b74215
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:55:18 -0500

    Create 3160-CalculateOrdersWithinEachInterval

---
+++ 3160-CalculateOrdersWithinEachInterval/3160-CalculateOrdersWithinEachInterval.sql
@@ -0,0 +1,16 @@
+-- Last updated: 8/20/2026, 1:55:18 AM
+with cte as (select
+    ceil(minute / 6) as interval_no,
+    order_count
+from
+    orders)
+
+select
+    interval_no,
+    sum(order_count) as total_orders
+from
+    cte
+group by
+    1
+order by
+    1;

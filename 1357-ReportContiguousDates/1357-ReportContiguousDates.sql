1313235909997809b77570cc132c29371654e0f7
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:04:06 -0500

    Create 1357-ReportContiguousDates

---
+++ 1357-ReportContiguousDates/1357-ReportContiguousDates.sql
@@ -0,0 +1,42 @@
+-- Last updated: 8/20/2026, 2:04:06 AM
+with cte as (select
+    fail_date as date,
+    'failed' as state
+from
+    failed
+union
+select
+    success_date as date,
+    'succeeded' as state
+from
+    succeeded),
+
+cte2 as (
+select
+    *,
+    row_number() over(order by date) as date_rnk,
+    row_number() over(partition by state order by date) as state_rnk
+from
+    cte),
+
+cte3 as (
+    select
+        *,
+        date_rnk - state_rnk as grp
+    from
+        cte2
+)
+
+select
+    state as period_state,
+    min(date) as start_date,
+    max(date) as end_date
+from
+    cte3
+where
+    year(date) = 2019
+group by
+    state, grp
+order by
+    start_date;
+

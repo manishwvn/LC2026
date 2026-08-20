20f68359373291b6eabd8a46ca0469f7135590e6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:11:15 -0500

    Create 578-GetHighestAnswerRateQuestion

---
+++ 578-GetHighestAnswerRateQuestion/578-GetHighestAnswerRateQuestion.sql
@@ -0,0 +1,13 @@
+-- Last updated: 8/20/2026, 2:11:15 AM
+select question_id as survey_log
+from (select 
+question_id,
+sum(case when action = 'answer' then 1 else 0 end) as ans_cnt,
+sum(case when action = 'show' then 1 else 0 end) as show_cnt
+
+
+from Surveylog
+group by 1) a
+
+order by  ans_cnt/show_cnt desc, 1
+limit 1

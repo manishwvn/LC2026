f30fed11b40ab8f460472e9083407c48c5e08685
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:10:57 -0500

    Create 602-FriendRequestsIiWhoHasTheMostFriends

---
+++ 602-FriendRequestsIiWhoHasTheMostFriends/602-FriendRequestsIiWhoHasTheMostFriends.sql
@@ -0,0 +1,25 @@
+-- Last updated: 8/20/2026, 2:10:57 AM
+with id_comb as(
+    select
+        requester_id as id,
+        accepter_id as friend_id
+    from
+        requestaccepted
+    union all
+    select
+        accepter_id,
+        requester_id
+    from
+        requestaccepted
+)
+
+select
+    id,
+    count(distinct friend_id) as num
+from
+    id_comb
+group by
+    id
+order by
+    num desc
+limit 1;

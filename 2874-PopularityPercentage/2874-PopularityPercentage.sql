5b3ab28fa23bc8d606a085db6b2b5f419ffad169
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:55:38 -0500

    Create 2874-PopularityPercentage

---
+++ 2874-PopularityPercentage/2874-PopularityPercentage.sql
@@ -0,0 +1,20 @@
+-- Last updated: 8/20/2026, 1:55:38 AM
+with cte as (select
+    *
+from
+    friends
+union
+select
+    user2 as user1,
+    user1 as user2
+from
+    friends)
+
+select
+    user1,
+    round(count(distinct user2) * 100.00 / (select count(distinct user1) from cte), 2)
+        as percentage_popularity
+from
+    cte
+group by
+    user1

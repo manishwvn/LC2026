e39efe867d281d3bfb2b9d6d5d86affe8c51d727
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:00:05 -0500

    Create 1914-FindTheSubtasksThatDidNotExecute

---
+++ 1914-FindTheSubtasksThatDidNotExecute/1914-FindTheSubtasksThatDidNotExecute.sql
@@ -0,0 +1,43 @@
+-- Last updated: 8/20/2026, 2:00:05 AM
+--  for each task there is a subtasks (subtask 1, 2, 3 ... 20) such that each task has subtask count (2 ... 20)
+
+--  from eg.
+--  for task = 1, subtask count = 3 and subtask = 2 executed -> 1, 3 not executed so need to display these
+
+-- key step -> convert tasks table in the form of (task_id, subtask_id) based on the number of counts.
+
+with recursive cte as(
+    select
+        task_id,
+        1 as subtask_id
+    from
+        tasks
+    union all
+    select
+        c.task_id,
+        c.subtask_id + 1
+    from
+        cte c
+    join
+        tasks t
+    on
+        c.task_id = t.task_id
+    where
+        c.subtask_id < t.subtasks_count
+)
+
+select
+    c.task_id,
+    c.subtask_id
+from
+    cte c 
+left join
+    executed e
+on
+    c.task_id = e.task_id
+    and
+    c.subtask_id = e.subtask_id
+where
+    e.subtask_id is null
+order by
+    1, 2

75c963492f3d140bfb18d31d708d51924475a288
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:57:16 -0500

    Create 2386-MinMaxGame

---
+++ 2386-MinMaxGame/2386-MinMaxGame.py
@@ -0,0 +1,16 @@
+# Last updated: 8/20/2026, 1:57:16 AM
+class Solution:
+    def minMaxGame(self, a: List[int]) -> int:
+        
+        def solve(n):
+            if n==1:
+                return
+            for i in range(n//2):
+                if i%2:
+                    a[i] = max (a[2*i], a[2*i+1])
+                else:
+                    a[i] = min (a[2*i], a[2*i+1])
+            solve(n//2)
+            return
+        solve(len(a))
+        return a[0]

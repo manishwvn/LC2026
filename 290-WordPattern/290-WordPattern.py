0415a69e6fe41d1f89ac32b53151743f98ae638c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:14:31 -0500

    Create 290-WordPattern

---
+++ 290-WordPattern/290-WordPattern.py
@@ -0,0 +1,26 @@
+# Last updated: 8/20/2026, 2:14:30 AM
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        map_index = {}
+        words = s.split()
+        
+        if len(pattern) != len(words):
+            return False
+        
+        for i in range(len(words)):
+            c = pattern[i]
+            w = words[i]
+
+            char_key = f"char_{c}"
+            char_word = f"word_{w}"
+            
+            if char_key not in map_index:
+                map_index[char_key] = i
+            
+            if char_word not in map_index:
+                map_index[char_word] = i 
+            
+            if map_index[char_key] != map_index[char_word]:
+                return False
+        
+        return True

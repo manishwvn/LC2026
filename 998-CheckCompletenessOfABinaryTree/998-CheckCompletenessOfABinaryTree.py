b1866d8a55b00706c9830b7c2c98115193039e3c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:07:12 -0500

    Create 998-CheckCompletenessOfABinaryTree

---
+++ 998-CheckCompletenessOfABinaryTree/998-CheckCompletenessOfABinaryTree.py
@@ -0,0 +1,27 @@
+# Last updated: 8/20/2026, 2:07:11 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+
+        queue = deque([root])
+
+        while queue:
+            node = queue.popleft()
+
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+
+            else:
+                while queue:
+                    node = queue.popleft()
+                    if node:
+                        return False
+
+        return True
+        

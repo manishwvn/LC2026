d117430280d21cc4b242708d081b476315283d26
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:11:08 -0500

    Create 586-CustomerPlacingTheLargestNumberOfOrders

---
+++ 586-CustomerPlacingTheLargestNumberOfOrders/586-CustomerPlacingTheLargestNumberOfOrders.sql
@@ -0,0 +1,18 @@
+-- Last updated: 8/20/2026, 2:11:07 AM
+with cte as (
+    select
+        customer_number,
+        count(customer_number) as counts,
+        dense_rank() over(order by count(customer_number) desc) as rnk 
+    from
+        orders
+    group by
+        customer_number
+)
+
+select
+    customer_number
+from
+    cte
+where
+    rnk = 1;

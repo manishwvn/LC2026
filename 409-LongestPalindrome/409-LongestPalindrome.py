0c929987ba7e699e46f6a5175cfeea36f28432f9
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:13:04 -0500

    Create 409-LongestPalindrome

---
+++ 409-LongestPalindrome/409-LongestPalindrome.py
@@ -0,0 +1,16 @@
+# Last updated: 8/20/2026, 2:13:04 AM
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        chars = set()
+        count = 0
+        for char in s:
+            if char in chars:
+                count += 2
+                chars.remove(char)
+            else:
+                chars.add(char)
+                
+        return count + 1 if len(chars) != 0 else count
+                
+        
+        

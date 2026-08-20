c136cd31dfc4578a058c836e708af6410f7b0b91
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:58:23 -0500

    Create 2204-FindSubsequenceOfLengthKWithTheLargestSum

---
+++ 2204-FindSubsequenceOfLengthKWithTheLargestSum/2204-FindSubsequenceOfLengthKWithTheLargestSum.py
@@ -0,0 +1,17 @@
+# Last updated: 8/20/2026, 1:58:23 AM
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        res = []
+
+        for index, num in enumerate(nums):
+            heappush(heap, (num, index))
+            if len(heap) > k:
+                heappop(heap)
+
+        while heap:
+            num, index = heappop(heap)
+            res.append(index)
+
+        res.sort()
+        return [nums[index] for index in res]

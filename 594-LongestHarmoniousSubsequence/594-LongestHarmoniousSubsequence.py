ca077f4380776a42d2d710c84cd67583a930c7c7
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:11:03 -0500

    Create 594-LongestHarmoniousSubsequence

---
+++ 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.py
@@ -0,0 +1,21 @@
+# Last updated: 8/20/2026, 2:11:03 AM
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+
+        counts = {}
+        max_len = 0
+
+        for num in nums:
+            if num in counts:
+                counts[num] += 1
+            else:
+                counts[num] = 1
+
+            if num - 1 in counts:
+                max_len = max(max_len, counts[num] + counts[num - 1])
+
+            if num + 1 in counts:
+                max_len = max(max_len, counts[num] + counts[num+1])
+
+        return max_len
+        

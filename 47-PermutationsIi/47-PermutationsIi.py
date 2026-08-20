47e5857089d5327372ccb7ab7be8a4cd88f98a19
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:19:27 -0500

    Create 47-PermutationsIi

---
+++ 47-PermutationsIi/47-PermutationsIi.py
@@ -0,0 +1,31 @@
+# Last updated: 8/20/2026, 2:19:27 AM
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        #using hashmap/counter
+        result = []
+        counts = Counter(nums)
+        
+        def helper(perm):
+            #base
+            if len(perm) == len(nums):
+                result.append(perm.copy())
+                return
+            
+            #logic
+            for n in counts:
+                #action
+                if counts[n] > 0:
+                    perm.append(n)
+                    counts[n] -= 1
+                    
+                    #recursion
+                    helper(perm)
+                    
+                    #backtrack
+                    counts[n] += 1
+                    perm.pop()
+                    
+        helper([])
+        return result
+        

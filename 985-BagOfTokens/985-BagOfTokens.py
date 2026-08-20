834297cc6250062dac749fd7701a04f23469bc3a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:07:16 -0500

    Create 985-BagOfTokens

---
+++ 985-BagOfTokens/985-BagOfTokens.py
@@ -0,0 +1,26 @@
+# Last updated: 8/20/2026, 2:07:16 AM
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        
+        queue = deque(sorted(tokens))
+        score, max_score = 0, 0 
+        
+        
+        while queue:
+            if power >= queue[0]:
+                t = queue.popleft()
+                power -= t
+                score += 1
+                max_score = max(score, max_score)
+            elif score > 0:
+                t = queue.pop()
+                power += t
+                score -= 1
+            
+            else: 
+                break
+                
+        return max_score
+                
+        
+        

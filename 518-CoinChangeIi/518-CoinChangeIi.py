af10ec8837c5c873f50ec2cebd64c3b7122a6b94
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:12:01 -0500

    Create 518-CoinChangeIi

---
+++ 518-CoinChangeIi/518-CoinChangeIi.py
@@ -0,0 +1,25 @@
+# Last updated: 8/20/2026, 2:12:01 AM
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        #1D DP space optimized
+        if not coins or len(coins) == 0:
+            return 0
+    
+        n, m = len(coins), amount
+
+        dp = [0] * (m + 1)
+        dp[0] = 1
+        
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if j >= coins[i - 1]:
+                    dp[j] = dp[j] + dp[j - coins[i - 1]]
+                    
+        return dp[-1]
+            
+        
+        
+                
+        
+        

d263c3d412ab05d3aa6a3ccbd49b99f964330a7b
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:15:20 -0500

    Create 229-MajorityElementIi

---
+++ 229-MajorityElementIi/229-MajorityElementIi.py
@@ -0,0 +1,29 @@
+# Last updated: 8/20/2026, 2:15:20 AM
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+
+        counts = defaultdict(int)
+
+        for num in nums:
+            counts[num] += 1
+
+            if len(counts) <= 2:
+                continue
+            
+            new_counts = defaultdict(int)
+            for num, count in counts.items():
+
+                if count > 1:
+                    new_counts[num] = count - 1
+            counts = new_counts
+                
+
+        res = []
+        for num in counts:
+            if nums.count(num) > len(nums) // 3:
+                res.append(num)
+        return res
+
+
+
+        

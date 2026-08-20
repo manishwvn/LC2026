5f43bdbf03007d396bc90df46de263ef89071beb
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:12:52 -0500

    Create 424-LongestRepeatingCharacterReplacement

---
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
@@ -0,0 +1,24 @@
+# Last updated: 8/20/2026, 2:12:52 AM
+from collections import defaultdict
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        #generalized
+        counts = defaultdict(int)
+        max_len = 0
+        max_freq = 0
+        l = 0
+
+        for r in range(len(s)):
+            counts[s[r]] += 1
+            max_freq = max(max_freq, counts[s[r]])
+
+            # window_size - max_freq = number of changes needed
+            while (r - l + 1) - max_freq > k:
+                counts[s[l]] -= 1
+                l += 1
+
+            max_len = max(max_len, r - l + 1)
+
+        return max_len

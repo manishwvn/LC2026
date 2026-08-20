9d25d02b5c1443bd70a3cdde41bcb6c47f72f26e
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:05:11 -0500

    Create 1236-NThTribonacciNumber

---
+++ 1236-NThTribonacciNumber/1236-NThTribonacciNumber.py
@@ -0,0 +1,7 @@
+# Last updated: 8/20/2026, 2:05:11 AM
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        a, b, c = 0, 1, 1
+        for _ in range(n):
+            a, b, c = b, c, a + b + c
+        return a

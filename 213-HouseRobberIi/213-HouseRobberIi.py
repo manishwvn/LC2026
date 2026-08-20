0f892dd03ff64b4b74b0640a3fe456b9b1f84471
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:15:43 -0500

    Create 213-HouseRobberIi

---
+++ 213-HouseRobberIi/213-HouseRobberIi.py
@@ -0,0 +1,10 @@
+# Last updated: 8/20/2026, 2:15:43 AM
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        a = b = c = d = 0
+        for i in range(len(nums)-1):
+            a, b = b, max(b, a + nums[i])
+            c, d = d, max(d, c + nums[i+1])
+        
+        return max(b, d, nums[0])
+        

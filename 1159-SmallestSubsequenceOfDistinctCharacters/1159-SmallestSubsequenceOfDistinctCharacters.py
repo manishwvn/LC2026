2692be15f0392ede552460d8ead87b577c175a64
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:05:56 -0500

    Create 1159-SmallestSubsequenceOfDistinctCharacters

---
+++ 1159-SmallestSubsequenceOfDistinctCharacters/1159-SmallestSubsequenceOfDistinctCharacters.py
@@ -0,0 +1,18 @@
+# Last updated: 8/20/2026, 2:05:56 AM
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        count, seen, stk = Counter(s), {s[0]}, [s[0]]
+        for curr in s[1:]:
+            if curr in seen:
+                count[curr] -=1
+                continue
+
+            while stk and count[stk[-1]] > 1 and stk[-1] > curr:                
+                seen.discard(stk[-1])
+                count[stk[-1]] -=1
+                stk.pop()
+
+            stk.append(curr)
+            seen.add(curr)
+                                
+        return "".join(stk)

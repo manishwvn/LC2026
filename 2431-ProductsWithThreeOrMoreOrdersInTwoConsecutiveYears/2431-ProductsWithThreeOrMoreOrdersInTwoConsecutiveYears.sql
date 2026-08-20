fe3859390233bb97eec6f9a159a6839c30fd7c26
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:57:05 -0500

    Create 2431-ProductsWithThreeOrMoreOrdersInTwoConsecutiveYears

---
+++ 2431-ProductsWithThreeOrMoreOrdersInTwoConsecutiveYears/2431-ProductsWithThreeOrMoreOrdersInTwoConsecutiveYears.sql
@@ -0,0 +1,21 @@
+-- Last updated: 8/20/2026, 1:57:05 AM
+with cte as(
+    select
+        product_id,
+        year(purchase_date),
+        count(*),
+        year(purchase_date) - row_number() over(partition by product_id order by year(purchase_date)) as diff
+    from
+        orders
+    group by product_id, year(purchase_date)
+    having count(*) >= 3)
+
+select
+    distinct
+    product_id
+from
+    cte
+group by
+    product_id, diff
+having
+    count(*) >= 2

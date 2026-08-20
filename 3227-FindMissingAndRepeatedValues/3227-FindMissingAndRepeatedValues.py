eba4f760399a2de275995b684fee6fcb21790cbf
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:55:04 -0500

    Create 3227-FindMissingAndRepeatedValues

---
+++ 3227-FindMissingAndRepeatedValues/3227-FindMissingAndRepeatedValues.py
@@ -0,0 +1,23 @@
+# Last updated: 8/20/2026, 1:55:04 AM
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+
+        n = len(grid)
+        freq = {}
+
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] in freq:
+                    freq[grid[i][j]] += 1
+                else:
+                    freq[grid[i][j]] = 1
+        
+        miss, repeat = -1, -1
+
+        for num in range(1, n*n + 1):
+            if num not in freq:
+                miss = num
+            elif freq[num] == 2:
+                repeat = num
+        
+        return [repeat, miss]

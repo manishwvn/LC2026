20ea2d6f5068bb980ca2d663938b7f3f3c53f4c2
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:05:50 -0500

    Create 1168-DuplicateZeros

---
+++ 1168-DuplicateZeros/1168-DuplicateZeros.py
@@ -0,0 +1,25 @@
+# Last updated: 8/20/2026, 2:05:50 AM
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+
+        zeroes = 0
+        for num in arr:
+            if num == 0:
+                zeroes += 1
+        
+        n = len(arr)
+        i = n - 1
+        j = n + zeroes - 1
+
+        while i != j:
+            if j < n:
+                arr[j] = arr[i]
+            j -= 1
+            
+            if arr[i] == 0:
+                if j < n:
+                    arr[j] = 0
+                j -= 1
+            i -= 1
+
+        

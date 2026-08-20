f58f7650de93eac90d7e4ac1e7b72810e97df825
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:04:10 -0500

    Create 1339-TeamScoresInFootballTournament

---
+++ 1339-TeamScoresInFootballTournament/1339-TeamScoresInFootballTournament.sql
@@ -0,0 +1,21 @@
+-- Last updated: 8/20/2026, 2:04:10 AM
+select
+    t.team_id,
+    t.team_name,
+    sum(
+        (case
+            when t.team_id = m.host_team and m.host_goals > m.guest_goals then 3
+            when t.team_id = m.guest_team and m.guest_goals > m.host_goals then 3
+            when m.host_goals = m.guest_goals then 1
+            else 0
+        end)) as num_points
+from
+    teams t
+left join
+    matches m
+on
+    t.team_id in (m.host_team, m.guest_team)
+group by
+    t.team_id
+order by
+    3 desc, 1;

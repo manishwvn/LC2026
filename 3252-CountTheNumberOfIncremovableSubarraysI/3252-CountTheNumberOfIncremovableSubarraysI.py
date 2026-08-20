d97841b4b085fa648de45f0957be14321db40879
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:55:03 -0500

    Create 3252-CountTheNumberOfIncremovableSubarraysI

---
+++ 3252-CountTheNumberOfIncremovableSubarraysI/3252-CountTheNumberOfIncremovableSubarraysI.py
@@ -0,0 +1,22 @@
+# Last updated: 8/20/2026, 1:55:03 AM
+class Solution:
+    def incremovableSubarrayCount(self, nums: List[int]) -> int:
+
+        def is_increase(arr):
+            for i in range(len(arr)-1):
+                if arr[i] >= arr[i+1]:
+                    return False
+            return True
+
+        result = 0
+
+        for l in range(len(nums)):
+            for r in range(l, len(nums)):
+                rem = nums[:l] + nums[r+1:]
+
+                if is_increase(rem):
+                    result += 1
+
+        return result
+
+        

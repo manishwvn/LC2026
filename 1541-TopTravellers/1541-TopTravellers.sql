71c89538e4384e9e0ce9866e86ba277a9dc4b63f
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:02:35 -0500

    Create 1541-TopTravellers

---
+++ 1541-TopTravellers/1541-TopTravellers.sql
@@ -0,0 +1,14 @@
+-- Last updated: 8/20/2026, 2:02:34 AM
+select
+    u.name,
+    ifnull(sum(r.distance),0) as travelled_distance
+from
+    users u
+left join
+    rides r
+on
+    u.id = r.user_id
+group by
+    u.id
+order by
+    travelled_distance desc, name asc;

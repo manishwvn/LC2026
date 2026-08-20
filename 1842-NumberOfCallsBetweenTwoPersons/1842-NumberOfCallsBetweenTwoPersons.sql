d9658a229f310c26ad2d7772b72e5d74d0aaea04
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:00:29 -0500

    Create 1842-NumberOfCallsBetweenTwoPersons

---
+++ 1842-NumberOfCallsBetweenTwoPersons/1842-NumberOfCallsBetweenTwoPersons.sql
@@ -0,0 +1,35 @@
+-- Last updated: 8/20/2026, 2:00:29 AM
+with cte as (select
+    from_id as person1,
+    to_id as person2,
+    duration
+from
+    calls
+union all
+select
+    to_id,
+    from_id,
+    duration
+from
+    calls)
+
+select
+    person1,
+    person2,
+    count(*) as call_count,
+    sum(duration) as total_duration
+from
+    cte
+where
+    person1 < person2
+group by
+    person1, person2;
+
+
+-- SELECT 
+--     LEAST(from_id, to_id) AS person1, 
+--     GREATEST(from_id, to_id) AS person2, 
+--     COUNT(*) AS call_count, 
+--     SUM(duration) AS total_duration
+-- FROM Calls
+-- GROUP BY 1, 2

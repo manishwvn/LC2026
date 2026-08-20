ca890bdb9e87581d04fe2cad2aa9342b5deebee6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:01:45 -0500

    Create 1666-MakeTheStringGreat

---
+++ 1666-MakeTheStringGreat/1666-MakeTheStringGreat.py
@@ -0,0 +1,18 @@
+# Last updated: 8/20/2026, 2:01:44 AM
+class Solution:
+    def makeGood(self, s: str) -> str:
+
+        if len(s) <= 1: return s
+
+        stack = []
+        for i in range(len(s)):
+            if stack and stack[-1] != s[i] and stack[-1].lower() == s[i].lower():
+                stack.pop()
+
+            else:
+                stack.append(s[i])
+
+        return "".join(stack)
+
+
+        

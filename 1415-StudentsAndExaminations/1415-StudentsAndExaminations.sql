8db643e4d50bb8bff95c2e96bb3cbc73f252ef95
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:03:43 -0500

    Create 1415-StudentsAndExaminations

---
+++ 1415-StudentsAndExaminations/1415-StudentsAndExaminations.sql
@@ -0,0 +1,21 @@
+-- Last updated: 8/20/2026, 2:03:43 AM
+select
+             
+            s.student_id,
+            s.student_name,
+            sub.subject_name,
+            count(e.student_id) as attended_exams
+    from
+        students s
+    join
+        subjects sub
+    
+    left join
+    examinations e 
+    on
+        s.student_id = e.student_id
+        and
+        e.subject_name = sub.subject_name
+
+    group by 1,2,3
+    order by 1, 3

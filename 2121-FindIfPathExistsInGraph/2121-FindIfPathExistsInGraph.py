4dac23f39bd038924c5530f6189df5173a48c866
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:58:53 -0500

    Create 2121-FindIfPathExistsInGraph

---
+++ 2121-FindIfPathExistsInGraph/2121-FindIfPathExistsInGraph.py
@@ -0,0 +1,23 @@
+# Last updated: 8/20/2026, 1:58:53 AM
+class Solution:
+    def validPath(
+        self, n: int, edges: List[List[int]], source: int, destination: int
+    ) -> bool:
+        
+        parent = [i for i in range(n)]
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                parent[rootX] = rootY
+
+        for a, b in edges:
+            union(a, b)
+
+        return find(source) == find(destination)

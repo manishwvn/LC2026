75dfdc787a11c10c20b5e814e0c478e342e3b501
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:57:25 -0500

    Create 2370-UsersWithTwoPurchasesWithinSevenDays

---
+++ 2370-UsersWithTwoPurchasesWithinSevenDays/2370-UsersWithTwoPurchasesWithinSevenDays.sql
@@ -0,0 +1,13 @@
+-- Last updated: 8/20/2026, 1:57:24 AM
+select 
+    distinct p1.user_id
+from 
+    purchases p1
+inner join
+    purchases p2
+on 
+    p1.user_id=p2.user_id 
+    and p1.purchase_id<>p2.purchase_id
+    and abs(datediff(p1.purchase_date, p2.purchase_date))<=7
+order by 
+    p1.user_id

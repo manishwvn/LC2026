12afdedcb87236851a11d085374127bc4bde8b80
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:53:52 -0500

    Create 3577-ConvertDoublyLinkedListToArrayI

---
+++ 3577-ConvertDoublyLinkedListToArrayI/3577-ConvertDoublyLinkedListToArrayI.py
@@ -0,0 +1,22 @@
+# Last updated: 8/20/2026, 1:53:52 AM
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+"""
+class Solution:
+    def toArray(self, root: 'Optional[Node]') -> List[int]:
+
+        arr = []
+
+        curr = root
+
+        while curr:
+            arr.append(curr.val)
+            curr = curr.next
+
+        return arr
+        

87d68a69ce7b78c2fd4f8ad48ac109bfb2bc1631
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:55:22 -0500

    Create 3155-HighestSalariesDifference

---
+++ 3155-HighestSalariesDifference/3155-HighestSalariesDifference.sql
@@ -0,0 +1,20 @@
+-- Last updated: 8/20/2026, 1:55:22 AM
+select distinct
+   abs((select max(salary) from salaries where department = 'Engineering') - 
+    (select max(salary) from salaries where department = 'Marketing'))
+    as salary_difference
+from
+    salaries;
+
+-- select
+--     abs(
+--     max(case
+--         when
+--             department = 'Engineering' then salary else 0 end) - 
+--     max(case
+--         when
+--             department = 'Marketing' then salary else 0 end)) as            
+--     salary_difference
+-- from
+--     salaries;
+    

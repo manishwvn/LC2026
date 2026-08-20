b00b8888a8c0226250187c30d53d23109112edab
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:06:14 -0500

    Create 1128-RemoveAllAdjacentDuplicatesInString

---
+++ 1128-RemoveAllAdjacentDuplicatesInString/1128-RemoveAllAdjacentDuplicatesInString.py
@@ -0,0 +1,19 @@
+# Last updated: 8/20/2026, 2:06:14 AM
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        
+        stack = []
+        result = ""
+        
+        for i in range(len(s)):
+            if not stack or stack[-1] != s[i]:
+                stack.append(s[i])
+                
+            else:
+                stack.pop()
+                
+        for char in stack:
+            result += char
+            
+        return result
+        

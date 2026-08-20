197d28e01e89afac3fc25be17459e554763e1366
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:13:40 -0500

    Create 366-FindLeavesOfBinaryTree

---
+++ 366-FindLeavesOfBinaryTree/366-FindLeavesOfBinaryTree.py
@@ -0,0 +1,31 @@
+# Last updated: 8/20/2026, 2:13:40 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+        result = []
+        
+        def helper(root):
+            if not root: return -1
+            
+            leftH = helper(root.left)
+            rightH = helper(root.right)
+            
+            maxH = max(leftH, rightH) + 1
+            
+            if maxH == len(result):
+                result.append([])
+                
+            result[maxH].append(root.val)
+            
+            return maxH
+            
+        helper(root)
+        
+        return result
+        

e24ad17851b02a1a396a5359fd18c6e735377390
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:09:54 -0500

    Create 678-ValidParenthesisString

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.py
@@ -0,0 +1,26 @@
+# Last updated: 8/20/2026, 2:09:54 AM
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        
+        min_, max_ = 0, 0 
+        
+        for char in s:
+            if char == '(':
+                min_ += 1
+                
+            else:
+                min_ -= 1
+                
+            if char != ")":
+                max_ += 1
+                
+            else:
+                max_ -= 1
+                
+            if max_ < 0: 
+                break
+                
+            min_ = max(min_, 0)
+        
+        return min_ == 0
+                

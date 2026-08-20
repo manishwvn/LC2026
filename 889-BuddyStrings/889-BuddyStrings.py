ff129a974f33b51d83e128d66f103eea437145a9
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:08:05 -0500

    Create 889-BuddyStrings

---
+++ 889-BuddyStrings/889-BuddyStrings.py
@@ -0,0 +1,18 @@
+# Last updated: 8/20/2026, 2:08:05 AM
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        
+        if len(s) != len(goal):
+            return False
+    
+        if s == goal:
+            return len(set(s)) < len(s)
+
+        diff = []
+        for i in range(len(s)):
+            if s[i] != goal[i]:
+                diff.append((s[i], goal[i]))
+
+        print(diff)
+        return len(diff) == 2 and diff[0] == diff[1][::-1]
+        

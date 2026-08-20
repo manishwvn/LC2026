ee04fc9da38b1cd4348d5b92a69a7d6570cf62eb
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:05:35 -0500

    Create 1193-GamePlayAnalysisV

---
+++ 1193-GamePlayAnalysisV/1193-GamePlayAnalysisV.sql
@@ -0,0 +1,20 @@
+-- Last updated: 8/20/2026, 2:05:35 AM
+with cte as (select
+    *,
+    dense_rank() over(partition by player_id order by event_date) as rnk,
+    lead(event_date,1) over(partition by player_id order by event_date) as nxt_login
+from
+    activity)
+
+select
+    event_date as install_dt,
+    count(distinct player_id) as installs,
+    round(sum(case when
+        datediff(nxt_login, event_date) = 1 then 1 
+    else 0 end) / count(distinct player_id), 2) as Day1_retention
+from
+    cte
+where
+    rnk = 1
+group by
+    event_date

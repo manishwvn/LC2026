d73ec464b78ca48944c2fcff497fd461e4718e62
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:04:24 -0500

    Create 1317-MonthlyTransactionsI

---
+++ 1317-MonthlyTransactionsI/1317-MonthlyTransactionsI.sql
@@ -0,0 +1,12 @@
+-- Last updated: 8/20/2026, 2:04:24 AM
+select
+    date_format(trans_date, '%Y-%m') as month,
+    country,
+    count(*) as trans_count,
+    count(case when state = 'approved' then 1 end) as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from
+    Transactions
+group by
+    month, country;

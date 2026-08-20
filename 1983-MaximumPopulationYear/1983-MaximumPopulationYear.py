dd524cf8d8c6ec540296351f3de841381e7058a8
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:59:41 -0500

    Create 1983-MaximumPopulationYear

---
+++ 1983-MaximumPopulationYear/1983-MaximumPopulationYear.py
@@ -0,0 +1,10 @@
+# Last updated: 8/20/2026, 1:59:41 AM
+class Solution:
+    def maximumPopulation(self, logs: List[List[int]]) -> int:
+        
+        count = {}
+        for i in logs:
+            for v in range(i[0],i[1]):
+                count[v] = count.get(v,0) + 1
+        m = max(count.values())
+        return min([i for i in count if count[i] == m])

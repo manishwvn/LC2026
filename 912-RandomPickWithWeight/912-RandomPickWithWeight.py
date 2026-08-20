383346d75bc1f6cf48fd1262206ea89dd864053a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:07:54 -0500

    Create 912-RandomPickWithWeight

---
+++ 912-RandomPickWithWeight/912-RandomPickWithWeight.py
@@ -0,0 +1,31 @@
+# Last updated: 8/20/2026, 2:07:54 AM
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.prefix_sums = []
+        sum_ = 0
+        for weight in w:
+            sum_ += weight
+            self.prefix_sums.append(sum_)
+        self.total_sum = sum_
+        
+    def pickIndex(self) -> int:
+        target = random.randint(1, self.total_sum)
+
+        l = 0
+        r = len(self.prefix_sums)
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if self.prefix_sums[mid] < target:
+                l = mid + 1
+            
+            else:
+                r = mid - 1
+        
+        return l
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()

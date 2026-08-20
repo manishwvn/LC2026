bc641dc7eb882a178c40ff3c032bf3f4f35b5a49
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:12:29 -0500

    Create 463-IslandPerimeter

---
+++ 463-IslandPerimeter/463-IslandPerimeter.py
@@ -0,0 +1,21 @@
+# Last updated: 8/20/2026, 2:12:29 AM
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        
+        m, n = len(grid), len(grid[0])
+        perimeter = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    
+                    if i > 0 and grid[i-1][j] == 1:
+                        perimeter -= 2
+                    
+                    
+                    if j > 0 and grid[i][j-1] == 1:
+                        perimeter -= 2
+                    
+        
+        return perimeter

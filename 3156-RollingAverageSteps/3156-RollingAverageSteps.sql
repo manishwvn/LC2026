d90fc9fa594caf2ca571a001d4ccff408d839c24
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:55:21 -0500

    Create 3156-RollingAverageSteps

---
+++ 3156-RollingAverageSteps/3156-RollingAverageSteps.sql
@@ -0,0 +1,13 @@
+-- Last updated: 8/20/2026, 1:55:21 AM
+
+WITH CTE AS
+(
+SELECT user_id, steps_date,
+ROUND(AVG(steps_count) OVER(PARTITION BY user_id ORDER BY steps_date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW),2) AS rolling_average,
+LAG(steps_date,2) OVER(PARTITION BY user_id ORDER BY steps_date) AS lag_date
+FROM Steps
+)
+SELECT user_id,steps_date, rolling_average
+FROM CTE
+WHERE DATEDIFF(steps_date, lag_date) = 2
+ORDER BY 1,2

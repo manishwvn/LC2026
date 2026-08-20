09abde150be42ed0beb73997d3d53a244865b5c7
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:54:24 -0500

    Create 3392-FindTrendingHashtags

---
+++ 3392-FindTrendingHashtags/3392-FindTrendingHashtags.sql
@@ -0,0 +1,14 @@
+-- Last updated: 8/20/2026, 1:54:24 AM
+with cte as (
+    select
+        CONCAT("#", SUBSTRING_INDEX(SUBSTRING_INDEX(tweet, "#", -1), " ", 1)) AS hashtag,
+        count(*) as hashtag_count
+    from
+        tweets
+    where
+        month(tweet_date) = 2 and year(tweet_date) = 2024
+    group by
+        hashtag
+)
+
+select * from cte order by hashtag_count desc, hashtag desc limit 3;

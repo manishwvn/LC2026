34bf49a331c5bb854f5eb49e5ecc13a44831fe7c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:53:26 -0500

    Create 3660-SecondHighestSalaryIi

---
+++ 3660-SecondHighestSalaryIi/3660-SecondHighestSalaryIi.sql
@@ -0,0 +1,16 @@
+-- Last updated: 8/20/2026, 1:53:25 AM
+# Write your MySQL query statement below
+
+with emp_sal_rk as
+(
+    select emp_id,
+    dept,
+    dense_rank() over (partition by dept order by salary desc) as dept_sal_rk
+    from 
+    employees
+)
+select emp_id,
+dept
+from emp_sal_rk
+where dept_sal_rk = 2
+order by 1

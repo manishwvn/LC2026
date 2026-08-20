8a44e551129698ba39c78d72de974d5840e37866
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:59:55 -0500

    Create 1942-PrimaryDepartmentForEachEmployee

---
+++ 1942-PrimaryDepartmentForEachEmployee/1942-PrimaryDepartmentForEachEmployee.sql
@@ -0,0 +1,23 @@
+-- Last updated: 8/20/2026, 1:59:55 AM
+with cte as (
+    select
+        employee_id,
+        count(department_id) as num_dept
+    from
+        employee 
+    group by
+        employee_id
+)
+
+select
+    e.employee_id,
+    e.department_id
+from
+    employee e
+join
+    cte c 
+on
+    e.employee_id = c.employee_id
+where
+    c.num_dept = 1
+    or (c.num_dept > 1 and e.primary_flag = 'Y');

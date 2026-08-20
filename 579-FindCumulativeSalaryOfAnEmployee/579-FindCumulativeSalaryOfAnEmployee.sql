25eafa75c83522b13b9c5435c5b7b7a0a94328c8
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:11:12 -0500

    Create 579-FindCumulativeSalaryOfAnEmployee

---
+++ 579-FindCumulativeSalaryOfAnEmployee/579-FindCumulativeSalaryOfAnEmployee.sql
@@ -0,0 +1,18 @@
+-- Last updated: 8/20/2026, 2:11:12 AM
+with cte as (
+    select
+        *,
+        row_number() over(partition by id order by month desc) as rnk
+    from
+        employee)
+
+
+select
+    id,
+    month,
+    sum(salary) over(partition by id order by month range between 2 preceding and current row) as Salary
+from
+    cte
+where rnk <> 1
+order by
+    id, month desc;

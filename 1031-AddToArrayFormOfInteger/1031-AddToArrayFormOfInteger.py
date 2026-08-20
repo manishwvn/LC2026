b9b0b8460a54a2ef122d42a7c4f20aa3558912b7
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:07:00 -0500

    Create 1031-AddToArrayFormOfInteger

---
+++ 1031-AddToArrayFormOfInteger/1031-AddToArrayFormOfInteger.py
@@ -0,0 +1,26 @@
+# Last updated: 8/20/2026, 2:06:59 AM
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+
+        num.reverse()
+        i = 0
+
+        while k:
+            d = k % 10
+            if i < len(num):
+                num[i] += d
+            else:
+                num.append(d)
+            
+            carry = num[i] // 10
+            num[i] = num[i] % 10
+
+            k //= 10
+            k += carry
+            i += 1
+        
+        num.reverse()
+        return num
+
+
+        

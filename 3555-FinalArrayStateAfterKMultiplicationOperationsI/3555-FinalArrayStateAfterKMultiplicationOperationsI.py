ef509cfbf8748c400ee693cbb04826ea427be5c2
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:53:56 -0500

    Create 3555-FinalArrayStateAfterKMultiplicationOperationsI

---
+++ 3555-FinalArrayStateAfterKMultiplicationOperationsI/3555-FinalArrayStateAfterKMultiplicationOperationsI.py
@@ -0,0 +1,16 @@
+# Last updated: 8/20/2026, 1:53:56 AM
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+
+        result = nums.copy()
+
+        heap = [(n, i) for i, n in enumerate(nums)]
+        heapq.heapify(heap)
+
+        for _ in range(k):
+            n, i = heapq.heappop(heap)
+            result[i] *= multiplier
+            heapq.heappush(heap, (result[i], i))
+
+        return result
+        

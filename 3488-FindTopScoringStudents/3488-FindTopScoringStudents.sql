371ba0b8f91722c5ec11f146e4340b0ce8afc82d
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:54:06 -0500

    Create 3488-FindTopScoringStudents

---
+++ 3488-FindTopScoringStudents/3488-FindTopScoringStudents.sql
@@ -0,0 +1,25 @@
+-- Last updated: 8/20/2026, 1:54:06 AM
+with cte as (select
+    s.*,
+    c.course_id
+from
+    students s
+join
+    courses c
+on
+    s.major = c.major)
+
+select
+    c.student_id
+from
+    cte c
+left join
+    enrollments e
+on
+    c.student_id = e.student_id
+    and
+    c.course_id = e.course_id
+group by
+    c.student_id
+having
+    count(distinct c.course_id) = sum(case when e.grade = 'A' then 1 else 0 end);

261d60f71b22b9dcba43ef2b8c6ecad9f5f266f8
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:18:13 -0500

    Create 98-ValidateBinarySearchTree

---
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
@@ -0,0 +1,27 @@
+# Last updated: 8/20/2026, 2:18:13 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        stack, prev = [], None
+        curr = root
+        
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            
+            curr = stack.pop()
+            if prev is not None and curr.val <= prev:
+                return False
+            prev = curr.val
+            curr = curr.right
+
+        return True
+                
+

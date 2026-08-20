03ad7ed8bfab21c6ed0507560bb3f7f658a4b89a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:15:51 -0500

    Create 207-CourseSchedule

---
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -0,0 +1,28 @@
+# Last updated: 8/20/2026, 2:15:51 AM
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        
+        graph = {i:[] for i in range(numCourses)}
+        indegree = [0] * numCourses
+        
+        for i, j in prerequisites:
+            graph[j].append(i)
+            indegree[i] += 1
+        
+        queue = []
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        count = 0
+        while queue:
+            node = queue.pop(0)
+            count += 1
+            
+            for neighbor in graph[node]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+        
+        return count == numCourses

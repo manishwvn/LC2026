c320e711d40f503d9fc598d23ee7bfa5a5edcd2c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:18:43 -0500

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.py
@@ -0,0 +1,24 @@
+# Last updated: 8/20/2026, 2:18:43 AM
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        
+        l, p, r = 0, 0, len(nums) - 1
+        
+        while p <= r:
+            
+            if nums[p] == 0:
+                nums[l], nums[p] = nums[p], nums[l]
+                l += 1
+                p += 1
+                
+                
+            elif nums[p] == 2:
+                nums[p], nums[r] = nums[r], nums[p]
+                r -= 1
+                
+            else:
+                p += 1
+                
+                
+                
+            

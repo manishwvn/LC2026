d0898f74fb464fbee2b996e2e30732f3e880e3dd
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:57:54 -0500

    Create 2293-MinimumNumberOfStepsToMakeTwoStringsAnagramIi

---
+++ 2293-MinimumNumberOfStepsToMakeTwoStringsAnagramIi/2293-MinimumNumberOfStepsToMakeTwoStringsAnagramIi.py
@@ -0,0 +1,21 @@
+# Last updated: 8/20/2026, 1:57:54 AM
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        c1, c2 = Counter(s), Counter(t)
+        
+        if len(s) == len(t) and c1 == c2:
+            return 0
+        
+        for char in t:
+            if char in c1:
+                c1[char] -= 1
+                if c1[char] == 0:
+                    c1.pop(char)
+                    
+                c2[char] -= 1
+                if c2[char] == 0:
+                    c2.pop(char)
+                
+        return sum(list(c1.values())) + sum(list(c2.values()))
+                
+        

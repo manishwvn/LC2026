b97e34e920ee12ec23668297bf22f7a41f15fe25
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:54:58 -0500

    Create 3259-BinaryTreeNodes

---
+++ 3259-BinaryTreeNodes/3259-BinaryTreeNodes.sql
@@ -0,0 +1,15 @@
+-- Last updated: 8/20/2026, 1:54:58 AM
+select
+    t1.n as N,
+    case
+        when
+            t1.p is null then 'Root'
+        when
+            not exists (select 1 from tree t2 where t2.p = t1.n) then 'Leaf'
+        else
+            'Inner'
+    end as Type
+from
+    tree t1
+order by
+    1;

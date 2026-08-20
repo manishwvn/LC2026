04c46529fb2aa69ecb517b35e0aa18a80b92374e
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:59:13 -0500

    Create 2064-LeetcodifySimilarFriends

---
+++ 2064-LeetcodifySimilarFriends/2064-LeetcodifySimilarFriends.sql
@@ -0,0 +1,15 @@
+-- Last updated: 8/20/2026, 1:59:12 AM
+SELECT DISTINCT 
+    l1.user_id AS user1_id, 
+    l2.user_id AS user2_id
+FROM Listens l1
+JOIN Listens l2
+ON 
+    l1.song_id = l2.song_id
+    AND l1.day = l2.day
+    AND l1.user_id < l2.user_id
+    AND (l1.user_id, l2.user_id) IN (SELECT * FROM Friendship)
+GROUP BY
+    l1.user_id, l2.user_id, l1.day
+HAVING 
+    COUNT(DISTINCT l1.song_id) >= 3

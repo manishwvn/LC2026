4d27350243396345761398b85e170d91265f1051
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:13:08 -0500

    Create 405-ConvertANumberToHexadecimal

---
+++ 405-ConvertANumberToHexadecimal/405-ConvertANumberToHexadecimal.py
@@ -0,0 +1,20 @@
+# Last updated: 8/20/2026, 2:13:08 AM
+class Solution:
+    def toHex(self, num: int) -> str:
+        hexas = "0123456789abcdef"
+        
+        if num == 0:
+            return "0"
+        if num > 0 and num <= 15:
+            return hexas[num]
+        
+        if num < 0:
+            num += 2**32
+        
+        result = []
+        
+        while num > 0:
+            result.append(hexas[num % 16])
+            num //= 16
+        
+        return ''.join(result[::-1])

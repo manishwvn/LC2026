4fe2999140b098a466e5d354252d4c1750673ac5
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:11:25 -0500

    Create 570-ManagersWithAtLeast5DirectReports

---
+++ 570-ManagersWithAtLeast5DirectReports/570-ManagersWithAtLeast5DirectReports.sql
@@ -0,0 +1,13 @@
+-- Last updated: 8/20/2026, 2:11:25 AM
+select
+    e1.name
+from
+    employee e1
+join
+    employee e2
+on
+    e1.id = e2.managerId
+group by
+    e1.id
+having
+    count(*) >= 5;

0366d38b9f6fc56c338ac5dff715a9e433f11fd9
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:00:23 -0500

    Create 1858-LatestTimeByReplacingHiddenDigits

---
+++ 1858-LatestTimeByReplacingHiddenDigits/1858-LatestTimeByReplacingHiddenDigits.py
@@ -0,0 +1,21 @@
+# Last updated: 8/20/2026, 2:00:23 AM
+class Solution:
+    def maximumTime(self, time: str) -> str:
+        s=list(time)
+        for i in range(len(s)):
+            if s[i]=='?':
+                if i==0:
+                    if s[i+1] in ['0','1','2','3','?']:
+                        s[i]='2'
+                    else:
+                        s[i]='1'
+                elif i==1:
+                    if s[i-1]=='1' or s[i-1]=='0':
+                        s[i]='9'
+                    else:
+                        s[i]='3'
+                elif i==3:
+                    s[i]='5'
+                elif i==4:
+                    s[i]='9'
+        return ''.join(s)

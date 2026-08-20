3365bf7d521485eac06c22a26358dfe04f684a6c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:10:40 -0500

    Create 614-SecondDegreeFollower

---
+++ 614-SecondDegreeFollower/614-SecondDegreeFollower.sql
@@ -0,0 +1,34 @@
+-- Last updated: 8/20/2026, 2:10:40 AM
+with followers as(
+    select
+        followee,
+        count(*) as follower_count
+    from
+        follow
+    group by
+        followee
+    having follower_count >= 1
+),
+
+followees as(
+    select
+        follower,
+        count(*) as followee_count
+    from
+        follow
+    group by
+        follower
+    having followee_count >= 1
+)
+
+select
+    fe.follower,
+    fr.follower_count as num
+from
+    followers fr
+join
+    followees fe
+on
+    fr.followee = fe.follower
+order by
+    fe.follower;

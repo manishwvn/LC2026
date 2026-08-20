a5a6b5b40231847d59f3d49d8f38dcfd0f4a41da
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:18:24 -0500

    Create 86-PartitionList

---
+++ 86-PartitionList/86-PartitionList.py
@@ -0,0 +1,31 @@
+# Last updated: 8/20/2026, 2:18:24 AM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        
+        less_head = ListNode(0)
+        greater_head = ListNode(0)
+
+        less = less_head
+        greater = greater_head
+        curr = head
+
+        while curr:
+            
+            if curr.val < x:
+                less.next = curr
+                less = less.next
+            
+            else:
+                greater.next = curr
+                greater = greater.next
+
+            curr = curr.next 
+
+        greater.next = None
+        less.next = greater_head.next
+        return less_head.next

0f8b3ca279f9a9acbad2d9f72f8b7ecefcfc692d
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:06:01 -0500

    Create 1150-TwoSumBsts

---
+++ 1150-TwoSumBsts/1150-TwoSumBsts.py
@@ -0,0 +1,35 @@
+# Last updated: 8/20/2026, 2:06:01 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
+        
+        def dfs(root, node_list):
+            if not root:
+                return
+            dfs(root.left, node_list)
+            node_list.append(root.val)
+            dfs(root.right, node_list)
+        
+        node_list1, node_list2 = [], []
+        dfs(root1, node_list1)
+        dfs(root2, node_list2)
+
+        l, r = 0, len(node_list2) - 1
+
+        while l < len(node_list1) and r > -1:
+            if node_list1[l] + node_list2[r] == target:
+                return True
+
+            elif node_list1[l] + node_list2[r] < target:
+                l += 1
+            
+            else:
+                r -= 1
+        return False
+
+    

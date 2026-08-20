b6c7247c1853d541709bfe53d4d70c7452f0863a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:58:55 -0500

    Create 2120-FirstAndLastCallOnTheSameDay

---
+++ 2120-FirstAndLastCallOnTheSameDay/2120-FirstAndLastCallOnTheSameDay.sql
@@ -0,0 +1,35 @@
+-- Last updated: 8/20/2026, 1:58:54 AM
+with cte as (
+    select
+        caller_id,
+        recipient_id,
+        call_time
+    from
+        calls
+    union
+    select
+        recipient_id,
+        caller_id,
+        call_time
+    from
+        calls
+),
+cte2 as (
+    select 
+        *,
+        dense_rank() over(partition by caller_id, date(call_time) order by call_time asc) as first_rnk,
+        dense_rank() over(partition by caller_id, date(call_time) order by call_time desc) as last_rnk
+    from cte)
+
+select
+    distinct caller_id as user_id
+from
+    cte2
+where
+    first_rnk = 1
+    or last_rnk = 1
+group by
+    user_id, date(call_time)
+having
+    count(distinct recipient_id) = 1
+

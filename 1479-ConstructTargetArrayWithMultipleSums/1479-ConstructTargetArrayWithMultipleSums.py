28284d04031912f961f2c1c6e272c77d951cde21
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:03:12 -0500

    Create 1479-ConstructTargetArrayWithMultipleSums

---
+++ 1479-ConstructTargetArrayWithMultipleSums/1479-ConstructTargetArrayWithMultipleSums.py
@@ -0,0 +1,28 @@
+# Last updated: 8/20/2026, 2:03:11 AM
+class Solution:
+    def isPossible(self, target: List[int]) -> bool:
+        
+        if len(target) == 1: return target == [1]
+        
+        total = sum(target)
+        
+        target = [-num for num in target]
+        heapify(target)
+        
+        while -target[0] != 1:
+            top = -heappop(target)
+            rest = total - top
+            
+            if rest == 1: return True
+            
+            val = top % rest
+            
+            if val == 0 or val == top: return False
+            
+            heappush(target, -val)
+            total = total - top + val
+            
+        return True
+            
+            
+        

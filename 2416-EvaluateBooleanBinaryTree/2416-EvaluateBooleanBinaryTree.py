fb8ca1d942ac1bf2db81c33a539e063b4d496d0a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:57:09 -0500

    Create 2416-EvaluateBooleanBinaryTree

---
+++ 2416-EvaluateBooleanBinaryTree/2416-EvaluateBooleanBinaryTree.py
@@ -0,0 +1,31 @@
+# Last updated: 8/20/2026, 1:57:09 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        
+        def helper(node):
+            if not node.left and not node.right:
+                if node.val == 1:
+                    return True
+                    
+                else:
+                    return False
+                    
+        
+            left = helper(node.left)
+            right = helper(node.right)
+            
+            if node.val == 2:
+                return left or right
+                
+            if node.val == 3:
+                return left and right
+        
+        return helper(root)
+        
+        

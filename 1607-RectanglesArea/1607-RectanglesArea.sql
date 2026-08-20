1b2880ec1eab26d85e58b59181c809da8a5539ae
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:02:12 -0500

    Create 1607-RectanglesArea

---
+++ 1607-RectanglesArea/1607-RectanglesArea.sql
@@ -0,0 +1,15 @@
+-- Last updated: 8/20/2026, 2:02:12 AM
+select
+    p1.id as p1,
+    p2.id as p2,
+    abs(p1.x_value - p2.x_value) * abs(p1.y_value - p2.y_value) as area
+from
+    points p1
+join
+    points p2
+on
+    p1.id < p2.id
+having
+    area <> 0
+order by
+    area desc, p1.id, p2.id;

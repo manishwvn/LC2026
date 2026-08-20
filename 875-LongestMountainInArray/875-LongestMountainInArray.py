93091a250de2854540b2fdeeadb029891a14fbf4
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:08:12 -0500

    Create 875-LongestMountainInArray

---
+++ 875-LongestMountainInArray/875-LongestMountainInArray.py
@@ -0,0 +1,23 @@
+# Last updated: 8/20/2026, 2:08:12 AM
+class Solution(object):
+    def longestMountain(self, A):
+        N = len(A)
+        ans = base = 0
+
+        while base < N:
+            end = base
+            if end + 1 < N and A[end] < A[end + 1]: #if base is a left-boundary
+                #set end to the peak of this potential mountain
+                while end+1 < N and A[end] < A[end+1]:
+                    end += 1
+
+                if end + 1 < N and A[end] > A[end + 1]: #if end is really a peak..
+                    #set 'end' to right-boundary of mountain
+                    while end+1 < N and A[end] > A[end+1]:
+                        end += 1
+                    #record candidate answer
+                    ans = max(ans, end - base + 1)
+
+            base = max(end, base + 1)
+
+        return ans

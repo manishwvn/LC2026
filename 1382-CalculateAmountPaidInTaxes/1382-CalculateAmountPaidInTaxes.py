67e7d19eaf95d4a50ecbeb4e2b538f1d26a23473
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:03:58 -0500

    Create 1382-CalculateAmountPaidInTaxes

---
+++ 1382-CalculateAmountPaidInTaxes/1382-CalculateAmountPaidInTaxes.py
@@ -0,0 +1,14 @@
+# Last updated: 8/20/2026, 2:03:58 AM
+class Solution:
+    def calculateTax(self, brackets: List[List[int]], income: int) -> float:
+        
+        brackets = [[0, 0]] + brackets 
+
+        total = 0
+        for i in range(1, len(brackets)):
+            
+            diff = min(income, brackets[i][0] - brackets[i - 1][0])
+            total += diff * (brackets[i][1] / 100)
+            income = max(income - diff, 0)
+            
+        return total

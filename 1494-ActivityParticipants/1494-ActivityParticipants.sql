b3763f37bb6621110ab375b8a5b7495ca63f1eb1
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:03:01 -0500

    Create 1494-ActivityParticipants

---
+++ 1494-ActivityParticipants/1494-ActivityParticipants.sql
@@ -0,0 +1,17 @@
+-- Last updated: 8/20/2026, 2:03:01 AM
+with cte as (select
+    activity,
+    dense_rank() over(order by count(id)) as min_rnk,
+    dense_rank() over(order by count(id) desc) as max_rnk
+from
+    friends
+group by
+    activity)
+
+select
+    activity
+from
+    cte
+where
+    min_rnk <> 1 and max_rnk <> 1;
+

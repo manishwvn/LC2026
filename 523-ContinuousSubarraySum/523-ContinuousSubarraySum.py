53fae9fbe860c8f6a3f60eb114ce9b030546fe5f
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:11:59 -0500

    Create 523-ContinuousSubarraySum

---
+++ 523-ContinuousSubarraySum/523-ContinuousSubarraySum.py
@@ -0,0 +1,21 @@
+# Last updated: 8/20/2026, 2:11:59 AM
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+    
+        hm = {0: -1}
+        prefix_sum = 0
+        
+        for i, num in enumerate(nums):
+            
+            prefix_sum = (prefix_sum + num) % k
+            
+            if prefix_sum not in hm:
+                hm[prefix_sum] = i
+            else:
+                if i - hm[prefix_sum] >= 2:
+                    return True
+        
+        return False
+            
+        
+        

eb9758ed7cd085badc77b4d4ccdf24f30acedb8f
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:56:19 -0500

    Create 2566-NumberOfUnequalTripletsInArray

---
+++ 2566-NumberOfUnequalTripletsInArray/2566-NumberOfUnequalTripletsInArray.py
@@ -0,0 +1,20 @@
+# Last updated: 8/20/2026, 1:56:19 AM
+class Solution:
+    def unequalTriplets(self, nums: List[int]) -> int:
+        freq = [0] * 1001  # Since nums[i] in 1 to 1000
+
+        for num in nums:
+            freq[num] += 1
+
+        total = 0
+        left = 0
+        n = len(nums)
+
+        for count_mid in freq:
+            if count_mid == 0:
+                continue
+            right = n - left - count_mid
+            total += left * count_mid * right
+            left += count_mid
+
+        return total

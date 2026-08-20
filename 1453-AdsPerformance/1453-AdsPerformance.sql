72df2ddbaf06e1f333e10853908b3cfa830c0ba3
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:03:29 -0500

    Create 1453-AdsPerformance

---
+++ 1453-AdsPerformance/1453-AdsPerformance.sql
@@ -0,0 +1,17 @@
+-- Last updated: 8/20/2026, 2:03:29 AM
+SELECT
+    ad_id,
+    ROUND(
+        IFNULL(
+            (SUM(CASE WHEN action = 'Clicked' THEN 1 ELSE 0 END) / 
+            (SUM(CASE WHEN action = 'Clicked' THEN 1 ELSE 0 END) + 
+             SUM(CASE WHEN action = 'Viewed' THEN 1 ELSE 0 END))), 
+        0) * 100, 2
+    ) AS ctr
+FROM
+    ads
+GROUP BY
+    ad_id
+ORDER BY
+    ctr DESC,
+    ad_id ASC;

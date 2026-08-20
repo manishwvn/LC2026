2f2aee6d72fc886e8ca3740511baff87ef2145d8
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:56:24 -0500

    Create 2547-OddStringDifference

---
+++ 2547-OddStringDifference/2547-OddStringDifference.py
@@ -0,0 +1,19 @@
+# Last updated: 8/20/2026, 1:56:23 AM
+class Solution:
+    def oddString(self, words: List[str]) -> str:
+        diffs = {}
+
+        for i, word in enumerate(words):
+            diff = []
+            for j in range(len(word) - 1):
+                diff.append(ord(word[j+1]) - ord(word[j]))
+            diff = tuple(diff)
+
+            if diff in diffs:
+                diffs[diff][0] += 1
+            else:
+                diffs[diff] = [1, i]
+
+        for count, idx in diffs.values():
+            if count == 1:
+                return words[idx]

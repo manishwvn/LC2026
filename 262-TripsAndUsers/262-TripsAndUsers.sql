3d486712eb66233efcf054f59fae9e57ee464e87
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:14:49 -0500

    Create 262-TripsAndUsers

---
+++ 262-TripsAndUsers/262-TripsAndUsers.sql
@@ -0,0 +1,28 @@
+-- Last updated: 8/20/2026, 2:14:49 AM
+with cancelled_trips as (
+    select
+        t.request_at as day,
+        case when t.status <> 'completed' then 1 else 0 end as cancelled
+    from
+        trips t
+    join
+        users u1
+    on
+        t.client_id = u1.users_id
+        and
+        u1.banned = 'No'
+    join
+        users u2
+    on
+        t.driver_id = u2.users_id
+        and
+        u2.banned = 'No'
+    where
+        date(request_at) between date('2013-10-01') and date('2013-10-03'))
+
+select
+    day,
+    round(sum(cancelled) / count(cancelled), 2) as 'Cancellation Rate'
+from
+    cancelled_trips
+group by day;

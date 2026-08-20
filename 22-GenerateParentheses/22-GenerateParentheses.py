6d62f4cf52389cea553ecb7d84c21a37882c4bd9
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:20:06 -0500

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -0,0 +1,26 @@
+# Last updated: 8/20/2026, 2:20:06 AM
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        #optimized backtracking
+        result = []
+
+        def generate(curr_string, left_count, right_count):
+            if len(curr_string) == 2 * n:
+                result.append("".join(curr_string))
+                return
+
+            if left_count < n:
+                curr_string.append('(')
+                generate(curr_string, left_count + 1, right_count)
+                curr_string.pop()
+
+            if right_count < left_count:
+                curr_string.append(')')
+                generate(curr_string, left_count, right_count + 1)
+                curr_string.pop()
+
+        curr_string = []
+        left_count, right_count = 0, 0 
+        generate(curr_string, left_count, right_count)
+        return result

b742de8127886601e8e43a5362dab2edbf62232d
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:54:56 -0500

    Create 3281-FindPeakCallingHoursForEachCity

---
+++ 3281-FindPeakCallingHoursForEachCity/3281-FindPeakCallingHoursForEachCity.sql
@@ -0,0 +1,28 @@
+-- Last updated: 8/20/2026, 1:54:56 AM
+with cte as (select
+    city,
+    hour(call_time) as hour,
+    count(*) as call_count
+from
+    calls
+group by
+    city, hour),
+
+cte2 as (
+    select
+        *,
+        dense_rank() over(partition by city order by call_count desc) as rnk
+    from
+        cte
+)
+
+select
+    city,
+    hour as peak_calling_hour,
+    call_count as number_of_calls
+from
+    cte2
+where
+    rnk = 1
+order by
+    2 desc, 1 desc;

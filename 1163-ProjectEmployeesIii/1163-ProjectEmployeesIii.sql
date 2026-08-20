62bf0bed436606a3fad65f2675b6a2213664cfe3
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:05:51 -0500

    Create 1163-ProjectEmployeesIii

---
+++ 1163-ProjectEmployeesIii/1163-ProjectEmployeesIii.sql
@@ -0,0 +1,18 @@
+-- Last updated: 8/20/2026, 2:05:51 AM
+select
+    project_id,
+    employee_id
+from
+    (
+    select
+        p.project_id,
+        p.employee_id,
+        dense_rank() over(partition by p.project_id order by e.experience_years desc) as rnk
+    from
+        project p 
+    join
+        employee e 
+    on
+        p.employee_id = e.employee_id) as t
+where
+    t.rnk = 1;

9fd2db31e57d6e03f78672280d61baaacf2884e4
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:16:03 -0500

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.py
@@ -0,0 +1,20 @@
+# Last updated: 8/20/2026, 2:16:03 AM
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        if len(nums) == 1: return nums[0]
+        
+        if len(nums) == 2: return max(nums[0], nums[1])
+        
+        
+        val0, val1 = nums[0], max(nums[0], nums[1])
+        result = 0
+        
+        for i in range(2, len(nums)):
+            result = max(val0 + nums[i], val1)
+            val0 = val1
+            val1 = result
+            
+        return result
+            
+        

3d16fc639cb9259e73a032766e480009c7e5e409
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:02:34 -0500

    Create 1542-ConsecutiveCharacters

---
+++ 1542-ConsecutiveCharacters/1542-ConsecutiveCharacters.py
@@ -0,0 +1,18 @@
+# Last updated: 8/20/2026, 2:02:34 AM
+class Solution:
+    def maxPower(self, s: str) -> int:
+
+        curr, max_count = 0, 0
+        prev = None
+
+        for char in s:
+            if char == prev:
+                curr += 1
+
+            else:
+                prev = char
+                curr = 1
+
+            max_count = max(curr, max_count)
+        return max_count
+

ef996bd8dd6861f4eb65fffa7af46b16b13bc699
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:59:58 -0500

    Create 1927-MaximumAscendingSubarraySum

---
+++ 1927-MaximumAscendingSubarraySum/1927-MaximumAscendingSubarraySum.py
@@ -0,0 +1,15 @@
+# Last updated: 8/20/2026, 1:59:58 AM
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+
+        curr_max, maxm = nums[0], nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                curr_max += nums[i]
+            else:
+                curr_max = nums[i]
+            maxm = max(curr_max, maxm)
+        return maxm
+
+        

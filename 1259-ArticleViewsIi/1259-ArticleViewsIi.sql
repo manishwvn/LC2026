ca35dbd5f528743c04ea1814e1cedb8e554cce9e
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:04:56 -0500

    Create 1259-ArticleViewsIi

---
+++ 1259-ArticleViewsIi/1259-ArticleViewsIi.sql
@@ -0,0 +1,15 @@
+-- Last updated: 8/20/2026, 2:04:56 AM
+select
+    distinct v1.viewer_id as id
+from
+    views v1
+join
+    views v2
+on
+    v1.viewer_id = v2.viewer_id
+and
+    v1.view_date = v2.view_date
+and
+    v1.article_id < v2.article_id
+order by
+    v1.viewer_id

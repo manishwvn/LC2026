d9df64c7dc9cf75621431eb8eb9f06d7f4bdd8c2
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:01:37 -0500

    Create 1688-TheMostRecentOrdersForEachProduct

---
+++ 1688-TheMostRecentOrdersForEachProduct/1688-TheMostRecentOrdersForEachProduct.sql
@@ -0,0 +1,26 @@
+-- Last updated: 8/20/2026, 2:01:37 AM
+with cte as (
+    select
+        o.order_id,
+        o.order_date,
+        o.product_id,
+        p.product_name,
+        dense_rank() over(partition by o.product_id order by o.order_date desc) as rnk
+    from
+        orders o
+    join
+        products p
+    on
+        o.product_id = p.product_id)
+
+select
+    product_name,
+    product_id,
+    order_id,
+    order_date
+from
+    cte
+where
+    rnk = 1
+order by
+    1,2,3

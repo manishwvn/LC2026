e5d319ffd53920b8937aa78d8b927ae48a243172
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:57:48 -0500

    Create 2302-OrderTwoColumnsIndependently

---
+++ 2302-OrderTwoColumnsIndependently/2302-OrderTwoColumnsIndependently.sql
@@ -0,0 +1,24 @@
+-- Last updated: 8/20/2026, 1:57:48 AM
+with cte1 as(
+    select
+        first_col,
+        row_number() over(order by first_col) as rnk
+    from
+        data),
+cte2 as(
+    select
+        second_col,
+        row_number() over(order by second_col desc) as rnk
+    from
+        data
+)
+
+select
+    cte1.first_col,
+    cte2.second_col
+from
+    cte1
+join
+    cte2
+on
+    cte1.rnk = cte2.rnk;

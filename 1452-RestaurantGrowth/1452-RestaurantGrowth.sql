578380730ddfb16aa3b65815950d295b34e283ea
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:03:31 -0500

    Create 1452-RestaurantGrowth

---
+++ 1452-RestaurantGrowth/1452-RestaurantGrowth.sql
@@ -0,0 +1,27 @@
+-- Last updated: 8/20/2026, 2:03:30 AM
+with cte as (
+    select
+        visited_on,
+        sum(amount) as daily_amount
+    from
+        customer
+    group by
+        visited_on),
+
+cte2 as (select
+    *,
+    sum(daily_amount) over(order by visited_on range between interval 6 day preceding and current row) as rolling_sum,
+    avg(daily_amount) over(order by visited_on range between interval 6 day preceding and current row) as rolling_avg
+from
+    cte)
+
+select
+    visited_on,
+    rolling_sum as amount,
+    round(rolling_avg, 2) as average_amount
+from
+    cte2
+where
+    visited_on >= (select min(visited_on) + interval 6 day from customer)
+order by
+    visited_on;

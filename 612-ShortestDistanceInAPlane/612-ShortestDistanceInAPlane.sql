337c7c7294a0fc6d96c67dcbe620200c097dc700
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:10:44 -0500

    Create 612-ShortestDistanceInAPlane

---
+++ 612-ShortestDistanceInAPlane/612-ShortestDistanceInAPlane.sql
@@ -0,0 +1,20 @@
+-- Last updated: 8/20/2026, 2:10:44 AM
+with distinct_points as(
+    select
+        p1.x as x1,
+        p1.y as y1,
+        p2.x as x2,
+        p2.y as y2
+    from
+        point2d p1
+    join
+        point2d p2
+    where
+        p1.x < p2.x OR (p1.x = p2.x AND p1.y < p2.y)
+)
+
+select
+    round(min(sqrt(pow((x1-x2), 2) + pow((y1-y2),2))), 2) as shortest
+from
+    distinct_points;
+

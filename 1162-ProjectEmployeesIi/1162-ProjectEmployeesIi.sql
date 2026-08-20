0e73e49421983fb8fe7b079a611d838bfc469a99
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:05:53 -0500

    Create 1162-ProjectEmployeesIi

---
+++ 1162-ProjectEmployeesIi/1162-ProjectEmployeesIi.sql
@@ -0,0 +1,14 @@
+-- Last updated: 8/20/2026, 2:05:53 AM
+select
+    project_id
+from
+    (
+select
+    project_id,
+    dense_rank() over(order by count(employee_id) desc) as rnk
+from
+    project p
+group by
+    project_id) t
+where
+    t.rnk = 1;

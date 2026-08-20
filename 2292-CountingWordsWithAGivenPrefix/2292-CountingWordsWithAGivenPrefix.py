856cbf2452cbf49b3464ac7d3cef7656c1cbae90
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:57:55 -0500

    Create 2292-CountingWordsWithAGivenPrefix

---
+++ 2292-CountingWordsWithAGivenPrefix/2292-CountingWordsWithAGivenPrefix.py
@@ -0,0 +1,37 @@
+# Last updated: 8/20/2026, 1:57:55 AM
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, word):
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+            curr.count += 1
+
+    def count(self, pref):
+        curr = self.root
+        for char in pref:
+            if char not in curr.children:
+                return 0
+            curr = curr.children[char]
+        return curr.count
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+
+
+        trie = Trie()
+
+        for word in words:
+            if len(word) >= len(pref):
+                trie.add(word)
+        return trie.count(pref)
+        

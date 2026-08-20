b643277f7c089088d4381cfe0ee674b6d0de21e4
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:17:19 -0500

    Create 131-PalindromePartitioning

---
+++ 131-PalindromePartitioning/131-PalindromePartitioning.py
@@ -0,0 +1,39 @@
+# Last updated: 8/20/2026, 2:17:18 AM
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        def is_pal(string):
+            start, end = 0, len(string) - 1
+            while start < end:
+                if string[start] != string[end]:
+                    return False
+                start += 1
+                end -= 1
+                
+            return True
+    
+        result = []
+
+        def helper(pos, path):
+            #base
+            if pos == len(s):
+                result.append(path.copy())
+                return 
+            
+            #logic
+            for i in range(pos, len(s)):
+                substr = s[pos:i+1]
+                
+                if is_pal(substr):
+                    #action
+                    path.append(substr)
+                    
+                    #recurse
+                    helper(i+1, path)
+                    
+                    #backtrack
+                    path.pop()
+        
+        helper(0, [])
+        return result
+        

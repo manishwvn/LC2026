ca51844b9aa20f7f74816a59910857f4bfafa797
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:10:11 -0500

    Create 652-FindDuplicateSubtrees

---
+++ 652-FindDuplicateSubtrees/652-FindDuplicateSubtrees.py
@@ -0,0 +1,28 @@
+# Last updated: 8/20/2026, 2:10:11 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+
+        subtrees = defaultdict(int)
+        result = []
+
+        def preorder(node):
+            if not node:
+                return "null"
+
+            serialized = ",".join([str(node.val), preorder(node.left), preorder(node.right)])
+            
+            subtrees[serialized] += 1
+
+            if subtrees[serialized] == 2:
+                result.append(node)
+            return serialized
+                
+        preorder(root)
+        return result
+        

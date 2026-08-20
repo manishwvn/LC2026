f083c81f252150a1f3c1cdf4a28e2499198f6186
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:04:44 -0500

    Create 1278-ProductPriceAtAGivenDate

---
+++ 1278-ProductPriceAtAGivenDate/1278-ProductPriceAtAGivenDate.sql
@@ -0,0 +1,24 @@
+-- Last updated: 8/20/2026, 2:04:44 AM
+with cte as (
+    select
+        *,
+        dense_rank() over(partition by product_id order by change_date desc) as rnk
+    from
+        products
+    where
+        change_date <= date('2019-08-16')
+)
+
+select
+    p.product_id,
+    coalesce(c.new_price, 10) as price 
+from
+    products p
+left join
+    cte c
+on
+    p.product_id = c.product_id
+    and
+    c.rnk = 1
+group by
+    p.product_id;

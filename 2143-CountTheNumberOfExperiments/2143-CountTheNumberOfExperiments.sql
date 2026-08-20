a84f9f0030a839dfc1348efdc4d8a955e5d10d6d
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:58:46 -0500

    Create 2143-CountTheNumberOfExperiments

---
+++ 2143-CountTheNumberOfExperiments/2143-CountTheNumberOfExperiments.sql
@@ -0,0 +1,26 @@
+-- Last updated: 8/20/2026, 1:58:45 AM
+WITH platforms AS (
+    SELECT 'Android' AS platform
+    UNION ALL
+    SELECT 'IOS' AS platform
+    UNION ALL
+    SELECT 'Web' AS platform
+),
+activities AS (
+    SELECT 'Reading' AS activity
+    UNION ALL
+    SELECT 'Sports' AS activity
+    UNION ALL
+    SELECT 'Programming' AS activity
+)
+
+SELECT
+    p.platform,
+    a.activity AS experiment_name,
+    SUM(CASE
+        WHEN ep.experiment_name = a.activity THEN 1 ELSE 0
+    END) AS num_experiments
+FROM platforms p
+JOIN activities a
+LEFT JOIN experiments ep ON p.platform = ep.platform AND a.activity = ep.experiment_name
+GROUP BY p.platform, a.activity;

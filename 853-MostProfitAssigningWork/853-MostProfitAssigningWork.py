8357e53edf36684979b5953b92281a6c566b217f
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:08:24 -0500

    Create 853-MostProfitAssigningWork

---
+++ 853-MostProfitAssigningWork/853-MostProfitAssigningWork.py
@@ -0,0 +1,49 @@
+# Last updated: 8/20/2026, 2:08:24 AM
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        
+        n, m = len(difficulty), len(worker)
+        
+        jobs = []
+        
+        for i in range(n):
+            jobs.append([difficulty[i], profit[i]])
+            
+        jobs.sort()
+        worker.sort()
+        
+        maxSoFar = [0] * n
+        maxSoFar[0] = jobs[0][1]
+        
+        for i in range(n):
+            maxSoFar[i] = max(maxSoFar[i-1], jobs[i][1])
+            
+        result = 0
+        
+        
+        for i in range(m):
+            curr = 0
+            
+            lo, hi = 0, n - 1
+            idx = -1
+            
+            while lo <= hi:
+                mid = (lo + hi) // 2
+                
+                if jobs[mid][0] <= worker[i]:
+                    idx = mid
+                    lo = mid + 1
+                    
+                else:
+                    hi = mid - 1
+                    
+            if idx != -1:
+                curr = maxSoFar[idx]
+                
+            result += curr
+            
+        return result
+            
+            
+                
+        

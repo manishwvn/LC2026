03329357f2c82e8795ef1e79fc86a94e2edd397b
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:58:58 -0500

    Create 2112-MinimumDifferenceBetweenHighestAndLowestOfKScores

---
+++ 2112-MinimumDifferenceBetweenHighestAndLowestOfKScores/2112-MinimumDifferenceBetweenHighestAndLowestOfKScores.py
@@ -0,0 +1,15 @@
+# Last updated: 8/20/2026, 1:58:58 AM
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        
+        nums.sort()
+        l = 0
+        curr, minm = 0, nums[-1] - nums[0] 
+        for r in range(l+k - 1, len(nums)):
+            curr = nums[r] - nums[l]
+            minm = min(curr, minm)
+            l += 1
+            
+        return minm
+        
+        

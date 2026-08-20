f0f001b659158821ff8c6216eda1b4f61d5f98e4
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:10:10 -0500

    Create 653-TwoSumIvInputIsABst

---
+++ 653-TwoSumIvInputIsABst/653-TwoSumIvInputIsABst.py
@@ -0,0 +1,35 @@
+# Last updated: 8/20/2026, 2:10:10 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        
+
+        def traverse(root):
+            nonlocal nums
+            if not root:
+                return
+            traverse(root.left)
+            nums.append(root.val)
+            traverse(root.right)
+
+        nums = []
+        traverse(root)
+        print(nums)
+
+        l, r = 0, len(nums) - 1
+        while l < r:
+            sum_ = nums[l] + nums[r]
+            if sum_ == k:
+                return True
+            elif sum_ > k:
+                r -= 1
+            else:
+                l += 1
+        return False
+
+            

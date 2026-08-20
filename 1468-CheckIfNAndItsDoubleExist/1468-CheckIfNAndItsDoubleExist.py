111f6df1ca03b1662da186f0022a655c01ebfa17
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:03:15 -0500

    Create 1468-CheckIfNAndItsDoubleExist

---
+++ 1468-CheckIfNAndItsDoubleExist/1468-CheckIfNAndItsDoubleExist.py
@@ -0,0 +1,15 @@
+# Last updated: 8/20/2026, 2:03:14 AM
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+
+        hm = {}
+
+        for i in range(len(arr)):
+            if (2 * arr[i]) in hm or (arr[i] % 2 == 0 and arr[i] // 2 in hm):
+                    return True
+                
+            else:
+                hm[arr[i]] = i
+
+        return False
+        

4c11f8257bfd86bb6526d3c0e1c240040a182d47
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:57:34 -0500

    Create 2331-IntersectionOfMultipleArrays

---
+++ 2331-IntersectionOfMultipleArrays/2331-IntersectionOfMultipleArrays.py
@@ -0,0 +1,20 @@
+# Last updated: 8/20/2026, 1:57:34 AM
+class Solution:
+    def intersection(self, nums: List[List[int]]) -> List[int]:
+        counts = {}
+        
+        for arr in nums:
+            for x in arr:
+                if x in counts:
+                    counts[x] += 1
+                else:
+                    counts[x] = 1
+                
+        ans = []
+        n = len(nums)
+        
+        for key in counts:
+            if counts[key] == n:
+                ans.append(key)
+                
+        return sorted(ans)

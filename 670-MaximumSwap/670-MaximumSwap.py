fad5c4eb2313324cc8b6218947e3c1716c15a1e0
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:09:59 -0500

    Create 670-MaximumSwap

---
+++ 670-MaximumSwap/670-MaximumSwap.py
@@ -0,0 +1,23 @@
+# Last updated: 8/20/2026, 2:09:58 AM
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        nums = [int(x) for x in str(num)]
+        n = len(nums)
+
+        # Track max digit from the right
+        max_idx = n - 1
+        left, right = -1, -1
+
+        for i in range(n - 2, -1, -1):
+            if nums[i] > nums[max_idx]:
+                max_idx = i
+            elif nums[i] < nums[max_idx]:
+                left = i
+                right = max_idx
+
+        if left != -1:
+            nums[left], nums[right] = nums[right], nums[left]
+        else:
+            return num
+
+        return int(''.join(str(x) for x in nums))

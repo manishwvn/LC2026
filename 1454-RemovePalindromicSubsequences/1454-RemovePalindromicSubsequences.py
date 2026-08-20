72c0435b38a42c9aa14a8fc5e33fa6a2ca4ef886
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:03:27 -0500

    Create 1454-RemovePalindromicSubsequences

---
+++ 1454-RemovePalindromicSubsequences/1454-RemovePalindromicSubsequences.py
@@ -0,0 +1,20 @@
+# Last updated: 8/20/2026, 2:03:27 AM
+class Solution:
+    def isPal(self, s):
+        l, r = 0, len(s) - 1
+        
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+            
+        return True
+            
+    def removePalindromeSub(self, s: str) -> int:
+        if not s: return 0
+        if self.isPal(s): return 1
+        return 2
+        
+        
+        

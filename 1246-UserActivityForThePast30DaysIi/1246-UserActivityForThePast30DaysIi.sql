e98c8465dc3b42f5d6491c9c3ff4725fb3beb3a2
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:05:07 -0500

    Create 1246-UserActivityForThePast30DaysIi

---
+++ 1246-UserActivityForThePast30DaysIi/1246-UserActivityForThePast30DaysIi.sql
@@ -0,0 +1,28 @@
+-- Last updated: 8/20/2026, 2:05:07 AM
+-- select
+--     sum(select count(distinct session_id from activity)) / count(user_id)
+-- from
+--     activity
+-- where
+--     activity_date between date('2019-07-27') - INTERVAL 29 DAY and date('2019-07-27')
+-- group by
+--     user_id
+-- having
+--     count(activity_type) >= 1;
+
+with user_sessions as(
+select
+    user_id,
+    count(distinct session_id) unique_sessions
+from
+    activity
+where
+    activity_date between date('2019-07-27') - INTERVAL 29 DAY and date('2019-07-27')
+group by
+    user_id
+having count(activity_type) >= 1)
+
+select
+    round(ifnull(sum(unique_sessions) / count(user_id), 0), 2) as average_sessions_per_user
+from
+    user_sessions;

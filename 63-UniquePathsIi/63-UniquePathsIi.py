c1281eb069ef51079273d517f6665cc61fd2e8e5
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:19:00 -0500

    Create 63-UniquePathsIi

---
+++ 63-UniquePathsIi/63-UniquePathsIi.py
@@ -0,0 +1,31 @@
+# Last updated: 8/20/2026, 2:19:00 AM
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+
+        if obstacleGrid[0][0] == 1:
+            return 0
+        
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        
+        obstacleGrid[0][0] = 1
+        for j in range(1, n):
+            obstacleGrid[0][j] = 0 if obstacleGrid[0][j] == 1 else obstacleGrid[0][j - 1]
+
+        for i in range(1, m):
+            obstacleGrid[i][0] = 0 if obstacleGrid[i][0] == 1 else obstacleGrid[i - 1][0]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 1:
+                    obstacleGrid[i][j] = 0
+
+                else:
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
+
+        return obstacleGrid[-1][-1]
+
+
+
+
+
+        

c8a462fd456acd801380d0175c5e62ad4979af79
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:02:27 -0500

    Create 1564-CreateASessionBarChart

---
+++ 1564-CreateASessionBarChart/1564-CreateASessionBarChart.sql
@@ -0,0 +1,21 @@
+-- Last updated: 8/20/2026, 2:02:27 AM
+WITH cte AS (
+    SELECT duration/60 AS mins
+    FROM sessions
+)
+
+SELECT '[0-5>' AS bin,
+       SUM(CASE WHEN mins<5 THEN 1 ELSE 0 END) AS total
+FROM cte
+UNION ALL
+SELECT '[5-10>' AS bin,
+       SUM(CASE WHEN mins>=5 AND mins<10 THEN 1 ELSE 0 END) AS total
+FROM cte
+UNION ALL
+SELECT '[10-15>' AS bin,
+       SUM(CASE WHEN mins>=10 AND mins<15 THEN 1 ELSE 0 END) AS total
+FROM cte
+UNION ALL
+SELECT '15 or more' AS bin,
+       SUM(CASE WHEN mins>=15 THEN 1 ELSE 0 END) AS total
+FROM cte

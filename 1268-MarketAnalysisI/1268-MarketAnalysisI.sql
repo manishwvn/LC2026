ed61ad9ce31e56111a067c334d40d0794ec6c632
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:04:53 -0500

    Create 1268-MarketAnalysisI

---
+++ 1268-MarketAnalysisI/1268-MarketAnalysisI.sql
@@ -0,0 +1,15 @@
+-- Last updated: 8/20/2026, 2:04:52 AM
+select
+    u.user_id as buyer_id,
+    u.join_date,
+    count(o.order_id) as orders_in_2019
+from
+    users u
+left join
+    orders o
+on
+    u.user_id = o.buyer_id
+and
+    year(o.order_date) = 2019
+group by
+    1

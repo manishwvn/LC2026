037ff39a059b4f4aeab830a422c1553d9161d94c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:09:10 -0500

    Create 768-PartitionLabels

---
+++ 768-PartitionLabels/768-PartitionLabels.py
@@ -0,0 +1,24 @@
+# Last updated: 8/20/2026, 2:09:10 AM
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        
+        if not s or len(s) == 0: return []
+        
+        result = []
+        
+        hm = {char:index for index, char in enumerate(s)}
+        
+        start, end = 0, 0
+        
+        for i in range(len(s)):
+            char = s[i]
+            end = max(end, hm[char])
+            
+            if i == end:
+                result.append(end - start + 1)
+                start = i + 1
+                
+        return result
+        
+        
+        

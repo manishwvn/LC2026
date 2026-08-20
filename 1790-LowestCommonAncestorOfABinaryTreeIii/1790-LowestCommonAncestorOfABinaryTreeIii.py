59c50065cd9e8db2aa43d7fd4efc9ad4dbf4d6cb
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:00:51 -0500

    Create 1790-LowestCommonAncestorOfABinaryTreeIii

---
+++ 1790-LowestCommonAncestorOfABinaryTreeIii/1790-LowestCommonAncestorOfABinaryTreeIii.py
@@ -0,0 +1,29 @@
+# Last updated: 8/20/2026, 2:00:51 AM
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+
+        p_node, q_node = p, q
+
+        while p_node != q_node:
+            if p_node:
+                p_node = p_node.parent
+            else:
+                p_node = q
+
+            if q_node:
+                q_node = q_node.parent
+            else:
+                q_node = p
+
+        return p_node
+        

dd86cf17d71d506a623516c9270cf5491ed12036
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:54:07 -0500

    Create 3481-SecondDayVerification

---
+++ 3481-SecondDayVerification/3481-SecondDayVerification.sql
@@ -0,0 +1,15 @@
+-- Last updated: 8/20/2026, 1:54:07 AM
+select
+    u.user_id
+from
+    emails u
+join
+    texts t
+on
+    u.email_id = t.email_id
+where
+    t.signup_action = 'Verified'
+and
+    date(t.action_date) = date_add(date(u.signup_date), interval 1 day)
+order by
+    1;

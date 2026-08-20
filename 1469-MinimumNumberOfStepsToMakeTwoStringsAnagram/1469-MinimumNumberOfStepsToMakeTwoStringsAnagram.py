64ab4dfb4250242cd25711fbc77abbb25feab508
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:03:13 -0500

    Create 1469-MinimumNumberOfStepsToMakeTwoStringsAnagram

---
+++ 1469-MinimumNumberOfStepsToMakeTwoStringsAnagram/1469-MinimumNumberOfStepsToMakeTwoStringsAnagram.py
@@ -0,0 +1,12 @@
+# Last updated: 8/20/2026, 2:03:12 AM
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        c1, c2 = collections.Counter(s), collections.Counter(t)
+        steps = 0
+        
+        for c in c1:
+            steps += max(0,(c1[c] - c2[c]))
+        
+        return steps
+                
+        

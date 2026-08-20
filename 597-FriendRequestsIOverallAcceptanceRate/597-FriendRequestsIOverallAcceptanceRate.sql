d8c18b79369cc2922f104e4544f47180f58485c1
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:11:00 -0500

    Create 597-FriendRequestsIOverallAcceptanceRate

---
+++ 597-FriendRequestsIOverallAcceptanceRate/597-FriendRequestsIOverallAcceptanceRate.sql
@@ -0,0 +1,14 @@
+-- Last updated: 8/20/2026, 2:11:00 AM
+SELECT
+    ROUND(
+        CASE
+            -- First, check if the denominator is zero
+            WHEN (SELECT COUNT(DISTINCT sender_id, send_to_id) FROM FriendRequest) = 0
+            THEN 0.00
+            -- If it's not zero, perform the division
+            ELSE
+                CAST((SELECT COUNT(DISTINCT requester_id, accepter_id) FROM RequestAccepted) AS FLOAT)
+                /
+                (SELECT COUNT(DISTINCT sender_id, send_to_id) FROM FriendRequest)
+        END
+    , 2) AS accept_rate;

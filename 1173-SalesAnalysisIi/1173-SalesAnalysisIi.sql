e35bd8444439c1b8b91bfaab3a60056ac3c727a2
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:05:47 -0500

    Create 1173-SalesAnalysisIi

---
+++ 1173-SalesAnalysisIi/1173-SalesAnalysisIi.sql
@@ -0,0 +1,14 @@
+-- Last updated: 8/20/2026, 2:05:47 AM
+SELECT DISTINCT s.buyer_id
+FROM Sales s
+JOIN Product p
+ON s.product_id = p.product_id
+AND p.product_name = 'S8'
+AND s.buyer_id NOT IN
+(
+    SELECT DISTINCT buyer_id
+    FROM Sales s
+    JOIN Product p
+    ON s.product_id = p.product_id
+    AND p.product_name = 'iPhone'
+    )

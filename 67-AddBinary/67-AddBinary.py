23a417ded28278cc91d3da3d53de622b9499e43a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:18:55 -0500

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.py
@@ -0,0 +1,26 @@
+# Last updated: 8/20/2026, 2:18:55 AM
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+
+        carry = 0
+        result = []
+        n = max(len(a), len(b))
+        a, b = a.zfill(n), b.zfill(n)
+
+        for i in range(n-1, -1, -1):
+            if a[i] == '1':
+                carry += 1
+            if b[i] == '1':
+                carry += 1
+
+            if carry % 2 == 1:
+                result.append('1')
+            else:
+                result.append('0')
+            carry //= 2
+
+        if carry == 1:
+            result.append('1')
+        result.reverse()
+
+        return ''.join(result)

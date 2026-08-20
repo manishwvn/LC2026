46d554ae7db8a32c4510ec990bca51d50d523939
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:08:29 -0500

    Create 841-ShortestDistanceToACharacter

---
+++ 841-ShortestDistanceToACharacter/841-ShortestDistanceToACharacter.py
@@ -0,0 +1,24 @@
+# Last updated: 8/20/2026, 2:08:29 AM
+class Solution:
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        n = len(s)
+        result = []
+        pos = -n
+        for i in range(n):
+            if s[i] == c:
+                pos = i
+            
+            result.append(i - pos)
+
+        print(result)
+        for i in range(n-1, -1, -1):
+            if s[i] == c:
+                pos = i
+
+            result[i] = min(result[i], abs(i - pos))
+
+        return result
+
+
+        
+        

e6e5b299f041a3d8e3d3100500e09038c6e56f2c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:00:50 -0500

    Create 1802-NumberOfStudentsUnableToEatLunch

---
+++ 1802-NumberOfStudentsUnableToEatLunch/1802-NumberOfStudentsUnableToEatLunch.py
@@ -0,0 +1,19 @@
+# Last updated: 8/20/2026, 2:00:50 AM
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+    
+        counts = [0, 0]
+        for s in students:
+            counts[s] += 1
+
+        rem = len(sandwiches)
+
+        for s in sandwiches:
+            if counts[s] == 0 or rem == 0:
+                break
+            counts[s] -= 1
+            rem -= 1
+        
+        return rem
+        
+        

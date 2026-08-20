526ecc67f15460270861b2582b2494ce648fc9ca
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:13:47 -0500

    Create 349-IntersectionOfTwoArrays

---
+++ 349-IntersectionOfTwoArrays/349-IntersectionOfTwoArrays.py
@@ -0,0 +1,18 @@
+# Last updated: 8/20/2026, 2:13:46 AM
+class Solution:
+    
+    def set_intersection(self, set1, set2):
+        return [x for x in set1 if x in set2]
+    
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        
+        
+        set1 = set(nums1)
+        set2 = set(nums2)
+        
+        if len(set1) < len(set2):
+            return self.set_intersection(set1, set2)
+        else:
+            return self.set_intersection(set2, set1)
+        

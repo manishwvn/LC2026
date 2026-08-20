e5a23d69c68a50a36315a9405913d002720b4da4
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:56:45 -0500

    Create 2474-MinimumAdjacentSwapsToMakeAValidArray

---
+++ 2474-MinimumAdjacentSwapsToMakeAValidArray/2474-MinimumAdjacentSwapsToMakeAValidArray.py
@@ -0,0 +1,21 @@
+# Last updated: 8/20/2026, 1:56:45 AM
+class Solution:
+    def minimumSwaps(self, nums: List[int]) -> int:
+        
+        minm, maxm = max(nums) + 1, -1
+        minIdx, maxIdx = -1, -1
+        
+        for i in range(len(nums)):
+            
+            if nums[i] >= maxm:
+                maxm = nums[i]
+                maxIdx = i
+                
+            if nums[i] < minm:
+                minm = nums[i]
+                minIdx = i
+        
+        if minIdx > maxIdx:
+            minIdx -= 1
+            
+        return (len(nums) - 1 - maxIdx) + (minIdx)

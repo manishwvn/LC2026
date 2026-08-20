7e1caa14046c68a18830c3baaf0f7a8cc830ea06
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:06:31 -0500

    Create 1083-TwoSumLessThanK

---
+++ 1083-TwoSumLessThanK/1083-TwoSumLessThanK.py
@@ -0,0 +1,25 @@
+# Last updated: 8/20/2026, 2:06:31 AM
+class Solution:
+    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+
+        if len(nums) == 1:
+            return -1
+
+        nums.sort()
+
+        max_sum = -1
+        for i in range(len(nums) - 1):
+            target = k - nums[i]
+            l, r = i + 1, len(nums) - 1
+            curr = -1
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] < target:
+                    curr = nums[mid]
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            if curr != -1:
+                max_sum = max(max_sum, nums[i] + curr)
+
+        return max_sum

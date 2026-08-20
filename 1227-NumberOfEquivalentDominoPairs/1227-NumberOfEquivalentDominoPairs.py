a32a2008c431ce6de7ab8b1af883e5d4bf806a17
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:05:16 -0500

    Create 1227-NumberOfEquivalentDominoPairs

---
+++ 1227-NumberOfEquivalentDominoPairs/1227-NumberOfEquivalentDominoPairs.py
@@ -0,0 +1,16 @@
+# Last updated: 8/20/2026, 2:05:16 AM
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        freq = [0] * 100
+        count = 0
+
+        for a, b in dominoes:
+            if a <= b:
+                code = 10 * a + b
+            else:
+                code = 10 * b + a 
+
+            count += freq[code]
+            freq[code] += 1
+
+        return count

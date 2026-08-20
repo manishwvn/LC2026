323e06453765f259c4bae4a4c0f4376ac9843b10
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:07:20 -0500

    Create 975-RangeSumOfBst

---
+++ 975-RangeSumOfBst/975-RangeSumOfBst.py
@@ -0,0 +1,33 @@
+# Last updated: 8/20/2026, 2:07:20 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        
+        sum_ = 0
+        
+        def helper(root):
+            nonlocal sum_
+            
+            if not root:
+                return
+            
+            
+                
+            if low <= root.val and high >= root.val:
+                sum_ += root.val
+
+            if low < root.val:
+                helper(root.left)
+
+            if root.val < high:
+                helper(root.right)
+            
+            
+        helper(root)
+        return sum_
+        

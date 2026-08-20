3e9d393f45bd9edbaa451799f95241eeb8ff2c98
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:15:18 -0500

    Create 230-KthSmallestElementInABst

---
+++ 230-KthSmallestElementInABst/230-KthSmallestElementInABst.py
@@ -0,0 +1,29 @@
+# Last updated: 8/20/2026, 2:15:17 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
+        def inorder(root):
+            nonlocal result, k
+
+            if not root:
+                return
+            
+            inorder(root.left)
+            k -= 1
+            if k == 0:
+                result = root.val
+                return
+                
+            inorder(root.right)
+
+        result = None
+        inorder(root)
+        return result
+
+        

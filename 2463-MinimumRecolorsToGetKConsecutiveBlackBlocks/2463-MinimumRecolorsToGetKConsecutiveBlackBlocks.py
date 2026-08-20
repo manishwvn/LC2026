4200c3db4c55c7ca9b103637703c98f9044ed541
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:56:48 -0500

    Create 2463-MinimumRecolorsToGetKConsecutiveBlackBlocks

---
+++ 2463-MinimumRecolorsToGetKConsecutiveBlackBlocks/2463-MinimumRecolorsToGetKConsecutiveBlackBlocks.py
@@ -0,0 +1,50 @@
+# Last updated: 8/20/2026, 1:56:48 AM
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+
+        # queue = deque()
+        # whites = 0
+
+        # for i in range(k):
+        #     if blocks[i] == "W":
+        #         whites += 1
+        #     queue.append(blocks[i])
+
+        # res = whites
+
+        # for i in range(k, len(blocks)):
+
+        #     if queue.popleft() == "W":
+        #         whites -= 1
+
+        #     if blocks[i] == "W":
+        #         whites += 1
+        #     queue.append(blocks[i])
+
+        #     res = min(whites, res)
+
+        # return res
+
+        whites = 0
+        res = float("inf")
+        l = 0
+        for r in range(len(blocks)):
+            if blocks[r] == "W":
+                whites += 1
+
+            if r - l + 1 == k:
+                res = min(res, whites)
+                if blocks[l] == "W":
+                    whites -= 1
+                l += 1
+
+        return res
+
+
+
+
+
+
+            
+            
+        

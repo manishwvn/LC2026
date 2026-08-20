8bba248046cafb2f34e8620cea08fa858e511263
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:16:32 -0500

    Create 170-TwoSumIiiDataStructureDesign

---
+++ 170-TwoSumIiiDataStructureDesign/170-TwoSumIiiDataStructureDesign.py
@@ -0,0 +1,31 @@
+# Last updated: 8/20/2026, 2:16:32 AM
+class TwoSum:
+
+    def __init__(self):
+        self.hm = {}
+        
+
+    def add(self, number: int) -> None:
+        if number in self.hm:
+            self.hm[number] += 1
+        else:
+            self.hm[number] = 1
+        
+
+    def find(self, value: int) -> bool:
+        for num in self.hm:
+            if value - num == num:
+                if self.hm[num] > 1:
+                    return True
+            else:
+                if value - num in self.hm:
+                    return True
+
+        return False
+        
+
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)

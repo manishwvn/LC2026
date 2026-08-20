fb70a8d02ee3825d9267cf798551ec5a54dcb242
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:55:28 -0500

    Create 3019-FurthestPointFromOrigin

---
+++ 3019-FurthestPointFromOrigin/3019-FurthestPointFromOrigin.py
@@ -0,0 +1,16 @@
+# Last updated: 8/20/2026, 1:55:28 AM
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+
+        l, r, _ = 0, 0, 0
+
+        for char in moves:
+            if char == 'L':
+                l += 1
+            elif char == 'R':
+                r += 1
+            else:
+                _ += 1
+        
+        return _ + abs(l - r)
+        

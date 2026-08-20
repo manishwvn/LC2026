01dbf1fa3af0abe20d8ea0e49fcdf093a29ce138
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:10:32 -0500

    Create 621-TaskScheduler

---
+++ 621-TaskScheduler/621-TaskScheduler.py
@@ -0,0 +1,25 @@
+# Last updated: 8/20/2026, 2:10:32 AM
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        
+        hm = Counter(tasks)
+        maxFreq = 0
+        maxCount = 0
+        
+        maxFreq = max(hm.values())
+        
+        for task, freq in hm.items():
+            if freq == maxFreq:
+                maxCount += 1
+                
+                
+        partitions = maxFreq - 1
+        available =  partitions * (n - (maxCount - 1))
+        pending = len(tasks) - (maxFreq * maxCount)
+        empty = max(0, available - pending)
+        
+        return len(tasks) + empty
+        
+        
+        
+        

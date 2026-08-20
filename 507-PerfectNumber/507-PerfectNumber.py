699c8df9d86d2cb5a9553ab0c3c33c516888711f
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:12:06 -0500

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.py
@@ -0,0 +1,15 @@
+# Last updated: 8/20/2026, 2:12:05 AM
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+
+        if num <=0: return False
+
+        sum_ = 0
+
+        for i in range(1, int(math.sqrt(num)) + 1):
+            if num % i == 0:
+                sum_ += i
+                if i * i != num:
+                    sum_ += num // i
+        return sum_ - num  == num
+        

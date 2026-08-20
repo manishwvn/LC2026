1009def5975279aa079f9f052b822e8a9739e929
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 02:09:28 -0500

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.py
@@ -0,0 +1,23 @@
+# Last updated: 8/20/2026, 2:09:27 AM
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        
+        stack = []
+        
+        for i in range(len(asteroids)):
+            
+            while stack and asteroids[i] < 0 < stack[-1]:
+                if stack[-1] < -asteroids[i]:
+                    stack.pop()
+                    continue
+                    
+                elif stack[-1] == -asteroids[i]:
+                    stack.pop()
+                break
+                    
+            else:
+                stack.append(asteroids[i])
+                    
+            
+        return stack
+        

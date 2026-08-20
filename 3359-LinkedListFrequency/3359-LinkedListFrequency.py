21d75e7eae6f70afc6373aa89e30123532c59476
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2026-08-20 01:54:32 -0500

    Create 3359-LinkedListFrequency

---
+++ 3359-LinkedListFrequency/3359-LinkedListFrequency.py
@@ -0,0 +1,27 @@
+# Last updated: 8/20/2026, 1:54:32 AM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def frequenciesOfElements(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        hm = {}
+
+        while head:
+            if head.val in hm:
+                hm[head.val] += 1
+            else:
+                hm[head.val] = 1
+            
+            head = head.next
+
+        new_head = ListNode()
+        curr = new_head
+
+        for k, v in hm.items():
+            curr.next = ListNode(v)
+            curr = curr.next
+        
+        return new_head.next
